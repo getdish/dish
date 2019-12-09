742351a98ce4990889ffac55ad2bdb7f88694a92
Majid Jabrayilov <cmecid@gmail.com>
2019-12-09 15:06:04 +0400

    Pager view refactored

--- apps/ios/new-dish/Shared/Views/PagerView.swift
+++ apps/ios/new-dish/Shared/Views/PagerView.swift
@@ -11,7 +11,7 @@ struct PagerView<Page: View>: View {
     @Binding var currentIndex: Int
     let pages: [Page]
 
-    @GestureState private var offset: CGFloat = 0
+    @GestureState private var translation: CGFloat = 0
 
     var body: some View {
         GeometryReader { geometry in
@@ -21,12 +21,12 @@ struct PagerView<Page: View>: View {
                         .frame(width: geometry.size.width)
                 }
             }
-            .offset(x: -(CGFloat(self.currentIndex) - self.offset) * geometry.size.width)
             .frame(width: geometry.size.width, alignment: .leading)
+            .offset(x: -CGFloat(self.currentIndex) * geometry.size.width + self.translation)
             .animation(.interactiveSpring())
             .gesture(
-                DragGesture().updating(self.$offset) { value, state, _ in
-                    state = value.translation.width / geometry.size.width
+                DragGesture().updating(self.$translation) { value, state, _ in
+                    state = value.translation.width
                 }.onEnded { value in
                     let offset = value.translation.width / geometry.size.width
                     let newIndex = (CGFloat(self.currentIndex) - offset).rounded()
