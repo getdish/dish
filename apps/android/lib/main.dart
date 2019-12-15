94cac843102c4edc7064a51fcf29eedfaeb0dad8
natew <natewienert@gmail.com>
2019-12-14 19:30:38 -0800

    configure for web

--- apps/android/lib/main.dart
+++ apps/android/lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You hav2e pushed the button this many times:',
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
