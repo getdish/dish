0542af16a5a342bba5936090bd5837d866dd170e
natew <natewienert@gmail.com>
2019-12-22 16:31:35 -0800

    save

--- apps/ios/new-dish/Model/RestaurantItem.swift
+++ apps/ios/new-dish/Model/RestaurantItem.swift
@@ -4,6 +4,10 @@ struct RestaurantItem: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     fileprivate var imageName: String
+    var address: String
+    var phone: String
+    var tags: [String]
+    var rating: Double
     var image: Image {
         ImageStore.shared.image(name: imageName)
     }
