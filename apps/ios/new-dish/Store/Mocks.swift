779303e3ae2220c873ee14103c8d8a53966c6d34
natew <natewienert@gmail.com>
2019-12-26 20:02:32 -0700

    save

--- apps/ios/new-dish/Store/Mocks.swift
+++ apps/ios/new-dish/Store/Mocks.swift
@@ -1,8 +1,3 @@
-class Constants {
-    static let homeInitialDrawerHeight = Screen.height * 0.7
-    static let homeInitialDrawerFullHeight = Screen.height - 70
-}
-
 class Mocks {
     static let homeSearchedPho = Store<AppState, AppAction>.init(
         initialState: AppState(
