8214f6442349dc79581350e2349d1353cb07dafa
natew <natewienert@gmail.com>
2019-12-26 23:48:45 -0700

    save

--- apps/ios/new-dish/SceneDelegate.swift
+++ apps/ios/new-dish/SceneDelegate.swift
@@ -9,12 +9,12 @@
 import UIKit
 import SwiftUI
 
+// Init state
+let appStore = Store<AppState, AppAction>.init(initialState: AppState(), reducer: appReducer)
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    
-    // Init state
-    let store = Store<AppState, AppAction>.init(initialState: AppState(), reducer: appReducer)
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -22,7 +22,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = RootView().environmentObject(store)
+        let contentView = RootView()
+            .environmentObject(appStore)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
--- apps/ios/new-dish/Store/LocationState.swift
+++ apps/ios/new-dish/Store/LocationState.swift
@@ -1,4 +1,9 @@
 import GooglePlaces
+import Combine
+import CoreLocation
+
+fileprivate let placesClient = GMSPlacesClient.shared()
+fileprivate let manager = CLLocationManager()
 
 extension AppState {
     struct LocationState: Equatable {
@@ -8,15 +13,17 @@ extension AppState {
         var lastKnown: CLLocation? = nil
         var isOnCurrent = false
         var hasChangedOnce = false
+        var likelyPlaces: [GMSPlace] = []
     }
 }
 
 enum LocationAction {
+    case listLikelyPlaces
+    case setLikelyPlaces(_ locations: [GMSPlace])
     case goToCurrent
     case setLastKnown(_ location: CLLocation?)
     case setSearch(_ search: String)
     case setSearchResults(_ locations: [CLLocation])
-
 }
 
 func locationReducer(_ state: inout AppState, action: LocationAction) {
@@ -29,5 +36,48 @@ func locationReducer(_ state: inout AppState, action: LocationAction) {
             state.location.searchResults = locations
         case let .setSearch(search):
             state.location.search = search
+        case let .setLikelyPlaces(locations):
+            state.location.likelyPlaces = locations
+        case .listLikelyPlaces:
+            state.location.likelyPlaces.removeAll()
+//            placesClient.currentPlace(callback: { (placeLikelihoods, error) -> Void in
+//                if let error = error {
+//                    // TODO: Handle the error.
+//                    print("Current Place error: \(error.localizedDescription)")
+//                    return
+//                }
+//                // Get likely places and add to the list.
+//                if let likelihoodList = placeLikelihoods {
+//                    for likelihood in likelihoodList.likelihoods {
+//                        let place = likelihood.place
+//                        state.location.likelyPlaces.append(place)
+//                    }
+//                }
+//            })
+    }
+}
+
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    
+    func start() {
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        appStore.send(.location(.setLastKnown(locations.last)))
+//        AppAction.location(.setLikelyPlaces(locations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            manager.startUpdatingLocation()
+            if !appStore.state.location.hasChangedOnce {
+                appStore.send(.location(.goToCurrent))
+            }
+        }
     }
 }
