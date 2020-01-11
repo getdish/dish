d98238444ed86cf5112c9a75c9e5cf7f57178254
natew <natewienert@gmail.com>
2020-01-11 14:18:38 -0800

    save

--- apps/ios/new-dish/Features/DishMap/DishMapView.swift
+++ apps/ios/new-dish/Features/DishMap/DishMapView.swift
@@ -7,20 +7,41 @@ struct DishMapView: View {
     var height: CGFloat
     var zoom: CGFloat = 12.0
     
-    @Environment(\.colorScheme) var colorScheme
     @EnvironmentObject var store: AppStore
     @EnvironmentObject var homeState: HomeViewState
     
     var body: some View {
-        return Color.red
-//        return MapView(
-//            width: width,
-//            height: height,
-//            zoom: zoom,
-//            darkMode: self.colorScheme == .dark,
-//            animate: homeState.dragState == .idle || self.homeState.y > self.homeState.aboutToSnapToBottomAt,
-//            location: store.state.location.isOnCurrent ? .current : .uncontrolled,
-//            locations: store.state.home.state.last!.searchResults.results.map { $0.place }
-//        )
+        DishMapViewContent(
+            width: width,
+            height: height,
+            zoom: zoom,
+            animate: homeState.dragState == .idle || self.homeState.y > self.homeState.aboutToSnapToBottomAt,
+            location: store.state.location.isOnCurrent ? .current : .uncontrolled,
+            locations: store.state.home.state.last!.searchResults.results.map { $0.place }
+        )
+    }
+}
+
+struct DishMapViewContent: View {
+    var width: CGFloat
+    var height: CGFloat
+    var zoom: CGFloat = 12.0
+    var animate: Bool = false
+    var location: MapViewLocation
+    var locations: [GooglePlaceItem]
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        print("!!!!!!!!!!!!!!!!!! render map view...")
+        return MapView(
+            width: width,
+            height: height,
+            zoom: zoom,
+            darkMode: self.colorScheme == .dark,
+            animate: animate,
+            location: location,
+            locations: locations
+        )
     }
 }
