0592a2ecd2504ada8a4cc43d39365b2a01b738dd
natew <natewienert@gmail.com>
2020-02-11 12:18:30 -0800

    simpler network for now

--- apps/ios/dish/App.swift
+++ apps/ios/dish/App.swift
@@ -6,26 +6,26 @@ import SwiftUI
 
 class AppModel {
     let store = Store<AppState, AppAction>.init(initialState: AppState(), reducer: appReducer)
-    
+
     // services
     let mapService = MapService()
     let homeService = HomeService()
     let googlePlacesService = GooglePlacesService()
-    
-//    let hasura = Hasura.getClient()
-    
+
+    let apollo = ApolloNetwork.shared
+
     // models
     let screen = ScreenModel()
-    
+
     // dynamic
     var enterRepl = false
-    
+
     // flags
     let enableMap = true
     let enableMapAutoZoom = false
     let enableContent = true
     let enableCamera = true
-    
+
     // constants
     let animationSpeed: Double = 1
     let keyboard = Keyboard()
@@ -37,7 +37,7 @@ class AppModel {
     let cameraButtonHeight: CGFloat = 44
     let filterBarHeight: CGFloat = 60
     let filterBarPad: CGFloat = 16
-    
+
     func start() {
         mapService.start()
         homeService.start()
--- apps/ios/dish/Service/ApolloNetwork.swift
+++ apps/ios/dish/Service/ApolloNetwork.swift
@@ -1,22 +1,8 @@
 import Foundation
 import Apollo
-import ApolloWebSocket
 
 class ApolloNetwork {
-    static let shared = ApolloNetwork()
-    let graphEndpoint = "https://hasura.io/learn/graphql"
-    var apolloClient : ApolloClient?
-    
-    private init (){
-    }
-    
-    func setApolloClient(accessToken: String){
-        self.apolloClient = {
-            let authPayloads = ["Authorization": "Bearer \(accessToken)"]
-            let configuration = URLSessionConfiguration.default
-            configuration.httpAdditionalHeaders = authPayloads
-            let endpointURL = URL(string: graphEndpoint)!
-            return ApolloClient(networkTransport: HTTPNetworkTransport(url: endpointURL, configuration: configuration))
-        }()
-    }
+  static let shared = ApolloNetwork()
+
+  private(set) lazy var apollo = ApolloClient(url: URL(string: "https://hasura.rio.dishapp.com/v1/graphql")!)
 }
