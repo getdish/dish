4a04b3c32e46ea23507cd776491582385cd04261
natew <natewienert@gmail.com>
2020-02-20 17:08:32 -0800

    move to place

---
+++ apps/ios/dish/Views/IconView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct IconView: View {
+    var background: AnyView = AnyView(Color.red)
+    var cornerRadius: CGFloat = 16
+    var image: Image = Image(systemName: "star")
+    var imageSize: CGFloat = 32
+    var padding: CGFloat = 12
+    var label: String = "Hello World"
+    var labelStyle: Text.Style = .iconLabel
+    
+    var body: some View {
+        VStack {
+            self.image
+                .resizable()
+                .scaledToFit()
+                .frame(width: self.imageSize, height: self.imageSize)
+                .padding(self.padding)
+                .background(self.background)
+                .clipShape(RoundedRectangle(cornerRadius: self.cornerRadius, style: .continuous))
+            
+            VStack(alignment: .center) {
+                Text(self.label)
+                    .style(self.labelStyle)
+                    .lineLimit(1)
+            }
+        }
+    }
+}
+
