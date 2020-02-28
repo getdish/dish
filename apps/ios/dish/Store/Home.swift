7d0618c9db2e2eff90a87e3736ae2f39a745c2d0
natew <natewienert@gmail.com>
2020-02-27 17:13:43 -0800

    save

--- apps/ios/dish/Store/Home.swift
+++ apps/ios/dish/Store/Home.swift
@@ -113,6 +113,7 @@ func homeReducer(_ state: inout AppState, action: HomeAction) {
         case .hideSplash:
             state.home.showSplash = false
         case .setSelectedMarkers(let markers):
+            let last = state.home.viewStates.last!
             if case .search(_, let results) = last.state {
                 state.home.viewStates.append(
                     HomeStateItem(
