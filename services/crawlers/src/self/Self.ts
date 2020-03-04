3d966096b18953c393ddb3c0c2e5147d3b1095ec
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-03-04 16:24:06 +0100

    crawlers: Update internal crawler
    To merge in the new crawler data we have

--- services/crawlers/src/self/Self.ts
+++ services/crawlers/src/self/Self.ts
@@ -13,7 +13,7 @@ const sleep = (ms: number) => new Promise(res => setTimeout(res, ms))
 const sanfran = {
   location: {
     _st_d_within: {
-      distance: 0.1,
+      distance: 0.5,
       from: {
         type: 'Point',
         coordinates: [-122.421351, 37.759251],
@@ -25,6 +25,9 @@ const sanfran = {
 export class Self extends WorkerJob {
   yelp!: Scrape
   ubereats!: Scrape
+  infatuated!: Scrape
+  michelin!: Scrape
+  tripadvisor!: Scrape
   restaurant!: Restaurant
 
   static queue_config: QueueOptions = {
@@ -53,7 +56,7 @@ export class Self extends WorkerJob {
       for (const result of results) {
         this.runOnWorker('mergeAll', [result.id])
         previous_id = result.id
-        if ((process.env.RUN_WITHOUT_WORKER = 'true')) {
+        if (process.env.RUN_WITHOUT_WORKER == 'true') {
           await sleep(100)
         }
       }
@@ -73,9 +76,6 @@ export class Self extends WorkerJob {
       await this.restaurant.delete()
       return
     }
-    if (!('data' in this.yelp)) {
-      return
-    }
     this.mergeName()
     this.mergeTelephone()
     this.mergeAddress()
@@ -90,58 +90,9 @@ export class Self extends WorkerJob {
   async getScrapeData() {
     this.yelp = await this.restaurant.getLatestScrape('yelp')
     this.ubereats = await this.restaurant.getLatestScrape('ubereats')
-  }
-
-  mergeName() {
-    this.restaurant.name = this.merge([
-      this.yelp.getData('data_from_map_search.name'),
-      this.ubereats.getData('main.title'),
-    ])
-  }
-
-  mergeTelephone() {
-    this.restaurant.telephone = this.merge([
-      this.yelp.getData('data_from_map_search.phone'),
-      this.ubereats.getData('main.phoneNumber'),
-    ])
-  }
-
-  mergeRatings() {
-    let ratings = [
-      this.yelp.getData('data_from_map_search.rating'),
-      this.ubereats.getData('main.ratingBadge[0].children[0].text'),
-    ]
-    ratings = ratings.map(r => parseFloat(r))
-    for (const rating of ratings) {
-      if (Number.isNaN(rating)) {
-        return
-      }
-    }
-    this.restaurant.rating = _.mean(ratings)
-  }
-
-  mergeAddress() {
-    const yelp_address_path =
-      'data_from_html_embed.mapBoxProps.addressProps.addressLines'
-    this.restaurant.address = this.merge([
-      this.yelp.getData(yelp_address_path, ['']).join(', '),
-      this.ubereats.getData('main.location.address'),
-    ])
-  }
-
-  mergeImage() {
-    const heroes = this.yelp.getData(
-      'data_from_html_embed.photoHeaderProps.photoHeaderMedias'
-    )
-    if (heroes) {
-      this.restaurant.image = heroes[0].srcUrl
-    }
-  }
-
-  mergeCategories() {
-    this.restaurant.categories = this.yelp
-      .getData('data_from_map_search.categories', [])
-      .map(c => c.title)
+    this.infatuated = await this.restaurant.getLatestScrape('infatuated')
+    this.michelin = await this.restaurant.getLatestScrape('michelin')
+    this.tripadvisor = await this.restaurant.getLatestScrape('tripadvisor')
   }
 
   // TODO: If we really want to be careful about being found out for scraping then we
@@ -165,6 +116,101 @@ export class Self extends WorkerJob {
     }
   }
 
+  mergeName() {
+    this.restaurant.name = this.merge([
+      this.yelp.getData('data_from_map_search.name'),
+      this.ubereats.getData('main.title'),
+      this.infatuated.getData('data_from_map_search.name'),
+      this.michelin.getData('main.name'),
+      this.tripadvisor.getData('overview.name'),
+    ])
+  }
+
+  mergeTelephone() {
+    this.restaurant.telephone = this.merge([
+      this.yelp.getData('data_from_map_search.phone'),
+      this.ubereats.getData('main.phoneNumber'),
+      this.infatuated.getData('data_from_html_embed.phone_number'),
+      this.tripadvisor.getData('overview.contact.phone'),
+    ])
+  }
+
+  mergeRatings() {
+    let ratings = [
+      this.yelp.getData('data_from_map_search.rating'),
+      this.ubereats.getData('main.ratingBadge[0].children[0].text'),
+      parseFloat(this.infatuated.getData('data_from_map_search.post.rating')) /
+        2,
+      this.tripadvisor.getData('overview.rating.primaryRating'),
+    ]
+    ratings = ratings.concat(this._getMichelinRating())
+    ratings = ratings.map(r => parseFloat(r))
+    for (const rating of ratings) {
+      if (Number.isNaN(rating)) {
+        return
+      }
+    }
+    this.restaurant.rating = _.mean(ratings)
+  }
+
+  private _getMichelinRating() {
+    const rating = this.michelin.getData('main.michelin_award')
+    switch (rating) {
+      case 'ONE_STAR':
+        return [5.0, 5.0]
+      case 'TWO_STARS':
+        return [5.0, 5.0, 5.0]
+      case 'THREE_STARS':
+        return [5.0, 5.0, 5.0, 5.0]
+      default:
+        return [5.0]
+    }
+  }
+
+  mergeAddress() {
+    const yelp_address_path =
+      'data_from_html_embed.mapBoxProps.addressProps.addressLines'
+    this.restaurant.address = this.merge([
+      this.yelp.getData(yelp_address_path, ['']).join(', '),
+      this.ubereats.getData('main.location.address'),
+      this.infatuated.getData('data_from_map_search.street'),
+      this.michelin.getData('main.title'),
+      this.tripadvisor.getData('overview.contact.address'),
+    ])
+  }
+
+  mergeImage() {
+    let hero = ''
+    const yelps = this.yelp.getData(
+      'data_from_html_embed.photoHeaderProps.photoHeaderMedias'
+    )
+    if (yelps) {
+      hero = yelps[0].srcUrl
+    }
+    const infatuateds = this.infatuated.getData(
+      'data_from_map_search.post.venue_image'
+    )
+    if (infatuateds) {
+      hero = infatuateds
+    }
+    const michelins = this.michelin.getData('main.image')
+    if (michelins) {
+      hero = michelins
+    }
+
+    this.restaurant.image = hero
+  }
+
+  mergeCategories() {
+    const yelps = this.yelp
+      .getData('data_from_map_search.categories', [])
+      .map(c => c.title)
+    const tripadvisors = this.tripadvisor
+      .getData('overview.detailCard.tagTexts.cuisines.tags', [])
+      .map(c => c.tagValue)
+    this.restaurant.categories = _.uniq(yelps.concat(tripadvisors))
+  }
+
   async upsertUberDishes() {
     if (!this.ubereats.id) {
       return
@@ -183,6 +229,13 @@ export class Self extends WorkerJob {
 
   mergePhotos() {
     let photos: string[] = []
+    photos = this._getYelpPhotos()
+    photos = photos.concat(this.tripadvisor.getData('photos', []))
+    this.restaurant.photos = photos
+  }
+
+  private _getYelpPhotos() {
+    let photos: string[] = []
     let page = 0
     let key: string
     while (true) {
@@ -197,10 +250,13 @@ export class Self extends WorkerJob {
       }
       page++
     }
-    this.restaurant.photos = photos
+    return photos
   }
 
   private static shortestString(arr: string[]) {
+    arr = arr.filter(el => {
+      return el != null && el != ''
+    })
     if (arr.length) {
       return arr.reduce((a, b) => (a.length <= b.length ? a : b))
     } else {
