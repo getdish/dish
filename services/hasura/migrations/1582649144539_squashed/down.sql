815613a66dc7a47eb19aae9a7ce34e61592c0e0e
natew <natewienert@gmail.com>
2020-03-09 22:10:59 -0700

    save

--- services/hasura/migrations/1582649144539_squashed/down.sql
+++
@@ -1,5 +0,0 @@
-
-alter table "public"."taxonomy" rename column "parentType" to "parenttype";
-alter table "public"."taxonomy" rename column "parentId" to "parentid";
-ALTER TABLE "public"."taxonomy" DROP COLUMN "order";
-ALTER TABLE "public"."taxonomy" DROP COLUMN "type";
