ace17d8d52a3e6c9becf44f7dc4358afea971858
natew <natewienert@gmail.com>
2020-03-13 14:17:58 -0700

    [web] show bottom sheet content

--- apps/web/shared/views/map/index.tsx
+++ apps/web/shared/views/map/index.tsx
@@ -3,13 +3,18 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import Map from './Map'
 import Bottom from './Bottom'
+import { View } from 'react-native'
 
 export const LabMap = () => {
   return (
-    <div>
+    <ZStack>
       <Map />
       <SearchBar />
       <Bottom />
-    </div>
+    </ZStack>
   )
+}
+
+function ZStack(props: { children: any }) {
+  return <View style={{ position: 'absolute' }}>{props.children}</View>
 }
