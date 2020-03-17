fb16585c66fd18677e3cad008332911cd7e57da9
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-03-17 13:51:26 +0100

    ci: Auto deploy for production branch commits

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Nothing to deploy yet :("
+PATH=$PATH:$HOME/bin
+KEY_PATH=$HOME/gitcrypt-key
+RIO_VERSION='0.7.0'
+RIO_BINARY=https://github.com/rancher/rio/releases/download/v$RIO_VERSION/rio-linux-amd64
+RIO_PATH=$HOME/bin/rio
+
+echo "Installing Rio v$RIO_VERSION..."
+mkdir -p $HOME/bin
+curl -sL $RIO_BINARY > $RIO_PATH
+chmod 755 $RIO_PATH
+rio --version
+
+echo "Decrypting Dish secrets..."
+sudo apt-get install -y git-crypt
+echo $GITCRYPT_KEY > $KEY_PATH.base64
+cat $KEY_PATH.base64 | base64 -d > $KEY_PATH
+git-crypt unlock $KEY_PATH
+
+echo "Deploying production branch to production..."
+mkdir -p $HOME/.kube
+cp -a k8s/etc/k8s_admin_creds.enc.config $HOME/.kube/config
+rio up --answers env.enc.production.yaml
