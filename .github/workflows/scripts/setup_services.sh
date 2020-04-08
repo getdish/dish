9c1f6a357ebba2ca0133b68d6b205550a908c72d
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-04-08 12:55:40 +0200

    [jwt-server] include in docker-compose.yaml
    This allows the test suite to be easily run locally

--- .github/workflows/scripts/setup_services.sh
+++ .github/workflows/scripts/setup_services.sh
@@ -15,22 +15,21 @@ export -f wait_until_hasura_ready
 
 mkdir -p $HOME/.dish/postgres/data
 docker-compose up -d
-sleep 10
+sleep 10 # Postgres needs 2 starts to get everything set up
 docker-compose down
 docker-compose up -d
 
+pushd services/hasura
 curl -L https://github.com/hasura/graphql-engine/raw/master/cli/get.sh | bash
+hasura migrate apply --endpoint $HASURA_ENDPOINT --admin-secret password
+popd
+
+# JWT server won't start until migrations have been applied
+docker-compose down
+docker-compose up -d
 
 if ! timeout --preserve-status 20 bash -c wait_until_hasura_ready; then
   echo "Timed out waiting for Hasura container to start"
   exit 1
 fi
 
-pushd services/hasura
-hasura migrate apply --endpoint $HASURA_ENDPOINT --admin-secret password
-popd
-
-pushd services/jwt-server
-yarn build
-yarn start &
-popd
