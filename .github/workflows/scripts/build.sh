df0075ec7f273bb11c261291f0bad89338a7cb5d
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-04-08 14:23:25 +0200

    [ci] seperate integration test into own CI run

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-docker build -t dish/worker -f services/worker/Dockerfile .
-docker build -t dish/crawlers -f services/crawlers/Dockerfile .
+sudo swapoff -a
+sudo rm -f /swapfile
+sudo apt clean
+df -h
+
+docker build -t dish/worker -f services/worker/Dockerfile . &
+docker build -t dish/crawlers -f services/crawlers/Dockerfile . &
+docker build -t dish/web -f apps/web/Dockerfile .
