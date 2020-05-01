0a900f2bd0ee32fccaf7d50fc9304def1479ec22
natew <natewienert@gmail.com>
2020-04-30 18:18:45 -0700

    [web] add react-error-boundary

--- apps/web/shared/views/ErrorBoundary.tsx
+++ apps/web/shared/views/ErrorBoundary.tsx
@@ -1,40 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary'
 import { Button } from 'react-native'
 
-import { Toast } from './Toast'
 import { ZStack } from './ui/Stacks'
 
-export class ErrorBoundary extends Component<{
+function ErrorFallback({ error, componentStack, resetErrorBoundary }) {
+  return (
+    <ZStack
+      fullscreen
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor="darkred"
+    >
+      <pre>{error.message}</pre>
+      <pre>{componentStack}</pre>
+      <hr />
+      <pre>{error.stack}</pre>
+      <Button title="Try Again" onPress={resetErrorBoundary}></Button>
+    </ZStack>
+  )
+}
+
+export function ErrorBoundary({
+  children,
+  name,
+}: {
+  children: any
   name: string
-  displayInline?: boolean
-}> {
-  state = {
-    error: null,
-  }
-
-  componentDidCatch(error) {
-    console.warn('ErrorBoundary caught error', this.props.name)
-    console.log(error.stack)
-    Toast.show(`Error in component: ${this.props.name}`)
-    this.setState({ error })
-  }
-
-  render() {
-    if (this.state.error) {
-      return (
-        <ZStack
-          fullscreen
-          alignItems="center"
-          justifyContent="center"
-          backgroundColor="darkred"
-        >
-          <Button
-            title="Retry"
-            onPress={() => this.setState({ error: null })}
-          ></Button>
-        </ZStack>
-      )
-    }
-    return this.props.children
-  }
+}) {
+  return (
+    <ReactErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => {
+        console.log('reset', name)
+        // can reset some app state here
+      }}
+    >
+      {children}
+    </ReactErrorBoundary>
+  )
 }
