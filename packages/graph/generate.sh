4895361f6ea61e14e64e1ae6d38694b41ba131b3
natew <natewienert@gmail.com>
2020-05-11 14:25:33 -0700

    [web] only generate if possible

--- packages/graph/generate.sh
+++ packages/graph/generate.sh
@@ -3,6 +3,10 @@
 set -e
 
 cd ../..
+if [ ! -f env.enc.production.yaml ]; then
+  echo "no env generated, exiting"
+  exit 0
+fi
 export HASURA_SECRET=$(\
   grep 'HASURA_GRAPHQL_ADMIN_SECRET:' env.enc.production.yaml \
     | tail -n1 | cut -c 30- | tr -d '"'\
