3deeeff926e4079251b1eee150d15d299000fd97
natew <natewienert@gmail.com>
2020-05-10 19:27:51 -0700

    [web] fix generate

--- packages/graph/generate.sh
+++ packages/graph/generate.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
-pushd $PROJECT_ROOT
+cd $PROJECT_ROOT
 export HASURA_SECRET=$(\
   grep 'HASURA_GRAPHQL_ADMIN_SECRET:' env.enc.production.yaml \
     | tail -n1 | cut -c 30- | tr -d '"'\
 )
-popd
+cd -
 
 node generate.js
