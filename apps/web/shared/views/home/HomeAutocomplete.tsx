47a7c647e05dab8d927d2694c3c4c522aca913d8
natew <natewienert@gmail.com>
2020-05-12 16:14:29 -0700

    [web] much nicer autocomplete style

--- apps/web/shared/views/home/HomeAutocomplete.tsx
+++ apps/web/shared/views/home/HomeAutocomplete.tsx
@@ -1,5 +1,5 @@
 import React, { memo, useEffect } from 'react'
-import { ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
+import { ScrollView, StyleSheet, Text, TouchableOpacity } from 'react-native'
 
 import {
   pageWidthMax,
@@ -11,13 +11,44 @@ import { Icon } from '../ui/Icon'
 import { LinearGradient } from '../ui/LinearGradient'
 import { LinkButton, LinkButtonProps } from '../ui/Link'
 import { HStack, ZStack } from '../ui/Stacks'
-import {
-  circularFlatButtonStyle,
-  flatButtonStyle,
-  flatButtonStyleActive,
-} from './baseButtonStyle'
+import { flatButtonStyle, flatButtonStyleActive } from './baseButtonStyle'
 import { SmallCircleButton } from './CloseButton'
 import { setAvoidNextAutocompleteShowOnFocus } from './HomeSearchBar'
+
+const useShowAutocomplete = () => {
+  const om = useOvermind()
+  const { showAutocomplete } = om.state.home
+  const showLocation = showAutocomplete == 'location'
+  const showSearch = showAutocomplete == 'search'
+  return showSearch || showLocation
+}
+
+export const HomeAutocompleteBackground = memo(() => {
+  const om = useOvermind()
+  const show = useShowAutocomplete()
+  return (
+    <ZStack
+      className="ease-in-out-fast"
+      opacity={show ? 1 : 0}
+      disabled={!show}
+      fullscreen
+      top={-10}
+      zIndex={11}
+    >
+      <TouchableOpacity
+        style={[StyleSheet.absoluteFill]}
+        onPress={() => {
+          om.actions.home.setShowAutocomplete(false)
+        }}
+      >
+        <LinearGradient
+          colors={['rgba(0,0,0,0.1)', 'transparent']}
+          style={[StyleSheet.absoluteFill, { height: 160 }]}
+        />
+      </TouchableOpacity>
+    </ZStack>
+  )
+})
 
 export default memo(function HomeAutoComplete() {
   const om = useOvermind()
@@ -55,142 +86,124 @@ export default memo(function HomeAutoComplete() {
     }
   }, [isShowing])
 
-  const gradient = (
-    <LinearGradient
-      colors={['rgba(0,0,0,0.1)', 'transparent']}
-      style={[StyleSheet.absoluteFill, { height: 160 }]}
-    />
-  )
-
   return (
-    <>
-      <ZStack
-        className="ease-in-out-fast"
-        opacity={isShowing ? 1 : 0}
-        disabled={!isShowing}
-        fullscreen
-        zIndex={11}
-      >
-        <TouchableOpacity
-          style={[StyleSheet.absoluteFill]}
-          onPress={() => {
-            om.actions.home.setShowAutocomplete(false)
-          }}
-        >
-          {gradient}
-        </TouchableOpacity>
-      </ZStack>
-      <ZStack
-        className="ease-in-out-fast"
-        position="absolute"
-        top={searchBarTopOffset + searchBarHeight}
-        left="50%"
-        right="5%"
-        maxWidth={pageWidthMax * 0.7}
-        zIndex={1000}
+    <ZStack
+      className="ease-in-out-fast"
+      position="absolute"
+      top={searchBarTopOffset + searchBarHeight}
+      left="2%"
+      right="2%"
+      zIndex={1000}
+      overflow="hidden"
+      paddingBottom={30}
+      paddingHorizontal={15}
+      opacity={isShowing ? 1 : 0}
+      transform={isShowing ? null : [{ translateY: 5 }]}
+      disabled={!isShowing}
+    >
+      <HStack
+        backgroundColor="rgba(0,0,0,0.6)"
+        className="blur"
+        paddingLeft={100}
+        // borderBottomRightRadius={12}
+        // borderBottomLeftRadius={12}
         overflow="hidden"
-        paddingBottom={30}
-        paddingHorizontal={15}
-        marginLeft={-500}
-        opacity={isShowing ? 1 : 0}
-        transform={isShowing ? null : [{ translateY: 5 }]}
-        disabled={!isShowing}
+        borderRadius={10}
+        height={49}
+        paddingBottom={1} // looks better 1px up
+        shadowColor="rgba(0,0,0,0.28)"
+        shadowRadius={18}
+        shadowOffset={{ width: 0, height: 3 }}
+        spacing
+        position="relative"
       >
-        <HStack
-          backgroundColor="rgba(255,255,255,0.95)"
-          // borderBottomRightRadius={12}
-          // borderBottomLeftRadius={12}
-          overflow="hidden"
-          borderRadius={10}
-          height={49}
-          paddingBottom={1} // looks better 1px up
-          shadowColor="rgba(0,0,0,0.28)"
-          shadowRadius={18}
-          shadowOffset={{ width: 0, height: 3 }}
-          spacing
-          position="relative"
-        >
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <HStack
-              height="100%"
-              flex={1}
-              alignItems="center"
-              paddingHorizontal={10}
-              spacing={6}
-            >
-              {autocompleteResultsActive.map((x, index) => {
-                const restaurantLinkProps: LinkButtonProps = x.type ==
-                  'restaurant' && {
-                  tag: null,
-                  name: 'restaurant',
-                  params: {
-                    slug: x.tagId,
-                  },
-                  onPress: () => {
-                    console.log('press')
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <HStack
+            height="100%"
+            flex={1}
+            alignItems="center"
+            paddingHorizontal={10}
+            spacing={6}
+          >
+            {autocompleteResultsActive.map((x, index) => {
+              const restaurantLinkProps: LinkButtonProps = x.type ==
+                'restaurant' && {
+                tag: null,
+                name: 'restaurant',
+                params: {
+                  slug: x.tagId,
+                },
+                onPress: () => {
+                  console.log('press')
+                  om.actions.home.setShowAutocomplete(false)
+                },
+              }
+
+              const plusButtonEl =
+                x.type === 'dish' && index !== 0 && om.state.user.isLoggedIn ? (
+                  <AutocompleteAddButton />
+                ) : null
+
+              const isActive = autocompleteIndex === index
+
+              return (
+                <LinkButton
+                  key={`${x.tagId}${index}`}
+                  onPress={() => {
+                    setAvoidNextAutocompleteShowOnFocus()
+                    if (showLocation) {
+                      om.actions.home.setLocation(x.name)
+                    }
+                    console.log('set false')
                     om.actions.home.setShowAutocomplete(false)
-                  },
-                }
-
-                const plusButtonEl =
-                  x.type === 'dish' &&
-                  index !== 0 &&
-                  om.state.user.isLoggedIn ? (
-                    <AutocompleteAddButton />
-                  ) : null
-
-                return (
-                  <LinkButton
-                    key={`${x.tagId}${index}`}
-                    onPress={() => {
-                      setAvoidNextAutocompleteShowOnFocus()
-                      if (showLocation) {
-                        om.actions.home.setLocation(x.name)
-                      }
-                      console.log('set false')
-                      om.actions.home.setShowAutocomplete(false)
-                    }}
-                    {...(!showLocation && {
-                      tag: x,
-                    })}
-                    flexDirection="row"
-                    fastClick
-                    alignItems="center"
-                    {...(isAutocompleteActive && autocompleteIndex === index
-                      ? flatButtonStyleActive
-                      : flatButtonStyle)}
-                    height={32}
-                    lineHeight={24}
-                    paddingHorizontal={10}
-                    fontSize={15}
-                    maxWidth="17vw"
-                    ellipse
-                    {...restaurantLinkProps}
-                  >
-                    {x.icon?.indexOf('http') === 0 ? (
-                      <img
-                        src={x.icon}
-                        style={{
-                          width: 16,
-                          height: 16,
-                          borderRadius: 100,
-                          marginRight: 10,
-                          marginTop: 'auto',
-                          marginBottom: 'auto',
-                        }}
-                      />
-                    ) : (
-                      x.icon ?? null
-                    )}
+                  }}
+                  {...(!showLocation && {
+                    tag: x,
+                  })}
+                  flexDirection="row"
+                  fastClick
+                  alignItems="center"
+                  height={32}
+                  lineHeight={24}
+                  paddingVertical={5}
+                  paddingHorizontal={10}
+                  borderRadius={10}
+                  backgroundColor={
+                    isActive ? 'rgba(0,0,0,0.35)' : 'transparent'
+                  }
+                  fontSize={15}
+                  maxWidth="17vw"
+                  hoverStyle={{
+                    backgroundColor: 'rgba(255,255,255,0.1)',
+                  }}
+                  ellipse
+                  {...restaurantLinkProps}
+                >
+                  {x.icon?.indexOf('http') === 0 ? (
+                    <img
+                      src={x.icon}
+                      style={{
+                        width: 16,
+                        height: 16,
+                        borderRadius: 100,
+                        marginRight: 10,
+                        marginTop: 'auto',
+                        marginBottom: 'auto',
+                      }}
+                    />
+                  ) : (
+                    x.icon ?? null
+                  )}
+                  <Text style={{ color: '#fff' }}>
                     {x.name} {plusButtonEl}
-                  </LinkButton>
-                )
-              })}
-            </HStack>
-          </ScrollView>
-        </HStack>
-      </ZStack>
-    </>
+                  </Text>
+                </LinkButton>
+              )
+            })}
+          </HStack>
+        </ScrollView>
+      </HStack>
+    </ZStack>
   )
 })
 
