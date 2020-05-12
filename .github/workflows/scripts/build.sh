ffc129637658d2f060c7a099752caa14893419a2
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-05-12 17:19:20 +0200

    [ci] free up ~25gb of space on github actions

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -4,6 +4,8 @@ set -e
 sudo swapoff -a
 sudo rm -f /swapfile
 sudo apt clean
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /usr/share/dotnet
 df -h
 
 docker build -t dish/worker -f services/worker/Dockerfile . &
