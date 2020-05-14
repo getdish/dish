8c078d3ac58d2892995762fbb5f77334a6614b9b
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-05-14 11:11:04 +0200

    [ci] em duplicate `yarn install` from Dockerfiles
    This was potentially causing `patch` to run twice causing build errors.
    But generally the Dockerfiles needed cleaning up anyway.

--- apps/web/etc/build_web.sh
+++ apps/web/etc/build_web.sh
@@ -5,12 +5,8 @@ export NODE_ENV=production
 
 yarn tsc -b tsconfig.build.json
 yarn build
-yarn build:web:client
-yarn build:web:server
 
-# TODO: A speed gain here would be nice, but it seems some "stream" conflicts
-# between them: when one deletes its stream the other complains.
-#parallel --halt now,fail=1 'bash -c {}' ::: \
-  #'yarn build:web:client' \
-  #'yarn build:web:server'
+parallel --halt now,fail=1 'bash -c {}' ::: \
+  'yarn build:web:client' \
+  'yarn build:web:server'
 
