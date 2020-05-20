3aa1f7686ca20227713770a76c744a51666d8df9
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-05-20 17:36:55 +0200

    WIP

---
+++ packages/graph/src/helpers/restaurant.ts
@@ -0,0 +1,212 @@
+import _ from 'lodash'
+
+import {
+  findOne,
+  insert,
+  levenshteinDistance,
+  update,
+  upsert,
+} from '../helpers'
+import { tagUpsert } from '../helpers/tag'
+import { query, resolved } from '../index'
+import { Restaurant, RestaurantTag, Tag } from '../types'
+
+export async function restaurantInsert(restaurants: Restaurant[]) {
+  return await insert<Restaurant>('restaurant', restaurants)
+}
+
+export async function restaurantUpsert(objects: Restaurant[]) {
+  return await upsert<Restaurant>(
+    'restaurant',
+    'restaurant_name_address_key',
+    objects
+  )
+}
+
+export async function restaurantUpdate(restaurant: Restaurant) {
+  return await update<Restaurant>('restaurant', restaurant)
+}
+
+export async function restaurantFindOne(restaurant: Partial<Restaurant>) {
+  return await findOne<Restaurant>('restaurant', restaurant)
+}
+
+export async function restaurantFindBatch(
+  size: number,
+  previous_id: string,
+  extra_where: {} = {}
+) {
+  return await resolved(() => {
+    return query.restaurant({
+      where: {
+        id: { _gt: previous_id },
+        ...extra_where,
+      },
+      order_by: { id: 'asc' },
+      limit: size,
+    })
+  })
+}
+
+export async function restaurantFindNear(
+  lat: number,
+  lng: number,
+  distance: number
+) {
+  return await resolved(() => {
+    return query.restaurant({
+      where: {
+        location: {
+          _st_d_within: {
+            distance: distance,
+            from: {
+              type: 'Point',
+              coordinates: [lng, lat],
+            },
+          },
+        },
+      },
+    })
+  })
+}
+
+export async function restaurantLatestScrape(
+  restaurant: Restaurant,
+  source: string
+) {
+  return await resolved(() => {
+    return query.scrape({
+      where: {
+        restaurant_id: {
+          _eq: restaurant.id,
+        },
+        source: {
+          _eq: source,
+        },
+      },
+      order_by: {
+        updated_at: 'desc',
+      },
+      limit: 1,
+    })
+  })
+}
+
+export async function restaurantSaveCanonical(
+  lon: number,
+  lat: number,
+  name: string,
+  street_address: string
+) {
+  const found = await findExistingCanonical(lon, lat, name)
+  if (found) return found
+  let restaurant = {
+    name,
+    address: street_address,
+    location: {
+      type: 'Point',
+      coordinates: [lon, lat],
+    },
+  } as Restaurant
+  restaurant = await restaurantInsert(restaurant)
+  if (process.env.RUN_WITHOUT_WORKER != 'true') {
+    console.log('Created new canonical restaurant: ' + restaurant.id)
+  }
+  return restaurant
+}
+
+async function findExistingCanonical(lon: number, lat: number, name: string) {
+  const nears = await restaurantFindNear(lat, lon, 0.0005)
+  let found: Restaurant | undefined = undefined
+  let shortlist = [] as Restaurant[]
+  let highest_sources_count = 0
+  for (const candidate of nears) {
+    if (candidate.name.includes(name) || name.includes(candidate.name)) {
+      shortlist.push(candidate)
+    }
+
+    if (levenshteinDistance(candidate.name, name) <= 3) {
+      shortlist.push(candidate)
+    }
+  }
+  if (shortlist.length == 0) return
+
+  found = shortlist[0]
+  for (const final of shortlist) {
+    const sources_count = Object.keys(final.sources || {}).length
+    if (sources_count > highest_sources_count) {
+      highest_sources_count = sources_count
+      found = final
+    }
+  }
+  if (process.env.RUN_WITHOUT_WORKER != 'true') {
+    console.log('Found existing canonical restaurant: ' + found.id)
+  }
+  return found
+}
+
+export async function restaurantRefresh(restaurant: Restaurant) {
+  return await restaurantFindOne({ id: restaurant.id })
+}
+
+export async function restaurantUpsertManyTags(
+  restaurant: Restaurant,
+  restaurant_tags: RestaurantTag[]
+) {
+  const populated = restaurant_tags.map((rt) => {
+    const existing = getRestaurantTagFromTag(restaurant, rt.tag_id)
+    return { ...existing, ...rt }
+  })
+  await restaurantUpsertRestaurantTag(restaurant, populated)
+  return await restaurantRefresh(restaurant)
+}
+
+export async function restaurantUpsertRestaurantTag(
+  restaurant: Restaurant,
+  restaurant_tags: RestaurantTag[]
+) {
+  await restaurantUpsertManyTags(restaurant, restaurant_tags)
+  await updateTagNames(restaurant)
+}
+
+export async function restaurantUpsertOrphanTags(
+  restaurant,
+  tag_strings: string[]
+) {
+  const tags = tag_strings.map((tag_name) => {
+    return {
+      name: tag_name,
+    } as Tag
+  })
+  const full_tags = await tagUpsert(tags)
+  const restaurant_tags = full_tags.map((tag: Tag) => {
+    return {
+      tag_id: tag.id,
+    } as RestaurantTag
+  })
+  await restaurantUpsertRestaurantTag(restaurant, restaurant_tags)
+}
+
+async function updateTagNames(restaurant: Restaurant) {
+  restaurant = await restaurantRefresh(restaurant)
+  const tag_names = restaurant.tags.map((i) => i.tag.slugs().flat())
+  restaurant.tag_names = _.uniq([
+    ...(restaurant.tag_names() || []),
+    ...tag_names,
+  ]) as string[]
+  return await restaurantUpdate(restaurant)
+}
+
+function getRestaurantTagFromTag(restaurant: Restaurant, tag_id: string) {
+  const existing = (restaurant.tags || []).find((i) => {
+    return i.tag.id == tag_id
+  })
+  let rt = {} as RestaurantTag
+  if (existing) {
+    const cloned = _.cloneDeep(existing)
+    delete cloned.tag
+    rt = cloned
+  }
+  rt.tag_id = tag_id
+  return rt
+}
--- packages/graph/src/types.ts
+++ packages/graph/src/types.ts
@@ -1,7 +1,17 @@
-import { dish, restaurant, review, tag, user } from './graphql'
+import {
+  dish,
+  restaurant,
+  restaurant_tag,
+  review,
+  tag,
+  tag_tag,
+  user,
+} from './graphql'
 
 export type Restaurant = Omit<restaurant, '__typename'>
 export type Tag = Omit<tag, '__typename'>
+export type RestaurantTag = Omit<restaurant_tag, '__typename'>
+export type TagTag = Omit<tag_tag, '__typename'>
 export type User = Omit<user, '__typename'>
 export type Review = Omit<review, '__typename'>
 export type Dish = Omit<dish, '__typename'>
