ec902b21b752fee9e463d1decd00545a1bbeecca
natew <natewienert@gmail.com>
2020-05-21 14:26:31 -0700

    [graph] add resolveAllFields for findOne

--- packages/models/src/flushTestData.ts
+++ packages/models/src/flushTestData.ts
@@ -1,13 +1,8 @@
-import { Dish } from './Dish'
-import { Review } from './Review'
-import { Scrape } from './Scrape'
-import { User } from './User'
-
 export async function flushTestData() {
-  await Scrape.deleteAllFuzzyBy('id_from_source', 'test')
-  await Review.deleteAllFuzzyBy('text', 'test')
+  // await Scrape.deleteAllFuzzyBy('id_from_source', 'test')
+  // await Review.deleteAllFuzzyBy('text', 'test')
   // await Tag.deleteAllFuzzyBy('name', 'test')
-  await User.deleteAllFuzzyBy('username', 'test')
-  await Dish.deleteAllFuzzyBy('name', 'Test')
+  // await User.deleteAllFuzzyBy('username', 'test')
+  // await Dish.deleteAllFuzzyBy('name', 'Test')
   // await Restaurant.deleteAllFuzzyBy('name', 'Test')
 }
