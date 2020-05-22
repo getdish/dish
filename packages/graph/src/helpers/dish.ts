9cbff9c9e6797a4f74a9d49bbb450a37e5d49727
natew <natewienert@gmail.com>
2020-05-22 15:24:02 -0700

    [graph] return tags (hacky for now)

--- packages/graph/src/helpers/dish.ts
+++ packages/graph/src/helpers/dish.ts
@@ -1,3 +1,4 @@
+import { dish_constraint } from '../graphql'
 import { Dish, DishWithId } from '../types'
 import { findOne, insert, update, upsert } from './queryHelpers'
 
@@ -5,13 +6,12 @@ export async function dishInsert(dish: Dish[]) {
   return await insert<Dish>('dish', dish)
 }
 
-// export async function dishUpsert(objects: Dish[]) {
-//   return await upsert<Dish>(
-//     'dish',
-//     '',
-//     objects
-//   )
-// }
+export async function dishUpsert(
+  objects: Dish[],
+  constraint = dish_constraint.dish_restaurant_id_name_key
+) {
+  return await upsert<Dish>('dish', constraint, objects)
+}
 
 export async function dishUpdate(dish: DishWithId) {
   return await update<DishWithId>('dish', dish)
