3f15f98faeee1f67d357e0e1fd7aca68802d5c67
natew <natewienert@gmail.com>
2020-05-29 08:08:05 -0700

    [web] fix build hanging

--- apps/web/etc/build_web.sh
+++ apps/web/etc/build_web.sh
@@ -5,7 +5,5 @@ export NODE_ENV=production
 
 yarn tsc -p tsconfig.build.json
 yarn build
-
-parallel --halt now,fail=1 'bash -c {}' ::: \
-  'yarn build:web:client' \
-  'yarn build:web:ssr'
+yarn build:web:client
+yarn build:web:ssr
