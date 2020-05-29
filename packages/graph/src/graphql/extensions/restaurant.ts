fd510e10ca60a0a1727ffc49e4e044c42442980a
natew <natewienert@gmail.com>
2020-05-29 10:46:40 -0700

    [web] start on using activeTagIds in RestaurantPeek

--- packages/graph/src/graphql/extensions/restaurant.ts
+++ packages/graph/src/graphql/extensions/restaurant.ts
@@ -1,22 +1,5 @@
-export const Query = {}
-
-/**
- * Add a key to a type
- */
-// export const User = {
-//   [GET_KEY]: (user) => user.id
-// }
-
-/**
- * Add custom data to a type
- * @example
- * query.users[0].follow()
- */
-// export const User = (user) => ({
-//   follow() {
-//     console.log('follow', user.id)
-//   }
-// })
+// 🚨
+// WARNING: using the types in this file slows builds down too much :(
 
 export type CarouselPhoto = {
   src: string
@@ -26,41 +9,52 @@ export type CarouselPhoto = {
 
 export const restaurant = (restaurant) => {
   return {
-    bestTagPhotos() {
-      const tags = restaurant.tags({ limit: 6 }) ?? []
-      return tags
-        ?.filter((t) => (t.photos?.length || 0) > 0 && t.rating)
-        .sort((t1, t2) => (t2.rating || 0) - (t1.rating || 0))
-    },
-
-    photosForCarousel() {
-      let photos = [] as CarouselPhoto[]
-      const max_photos = 4
-      const tags = restaurant.tags({ limit: 6 }) ?? []
-      for (const t of tags) {
-        const [photo] = t.photos ?? []
-        if (!photo) {
-          continue
-        }
-        let photo_name = t.tag.name || ' '
-        if (t.tag.icon) {
-          photo_name = t.tag.icon + photo_name
-        }
-        photos.push({
-          name: photo_name,
-          src: photo,
-          rating: t.rating,
-        })
-        if (photos.length >= max_photos) break
-      }
-      if (photos.length <= max_photos) {
-        const restPhotos = restaurant.photos() ?? []
-        for (const photo of restPhotos) {
-          photos.push({ name: ' ', src: photo })
-          if (photos.length >= max_photos) break
-        }
-      }
-      return photos
-    },
+    bestTagPhotos: () => restaurantBestTagPhotos(restaurant),
+    photosForCarousel: (activeTagIds: string[]) =>
+      restaurantPhotosForCarousel(restaurant, activeTagIds),
   }
+}
+
+function restaurantBestTagPhotos(restaurant) {
+  const tags = restaurant.tags({ limit: 6 }) ?? []
+  return tags
+    ?.filter((t) => (t.photos?.length || 0) > 0 && t.rating)
+    .sort((t1, t2) => (t2.rating || 0) - (t1.rating || 0))
+}
+
+function restaurantPhotosForCarousel(restaurant: any, activeTagIds: string[]) {
+  let photos = [] as CarouselPhoto[]
+  const max_photos = 4
+  const tags = restaurant.tags({
+    limit: 6,
+    // TODO @tom using activeTagIds to find matching tags here
+    // but fall back in case not existing
+    // where: {
+
+    // }
+  })
+  for (const t of tags) {
+    const [photo] = t.photos ?? []
+    if (!photo) {
+      continue
+    }
+    let photo_name = t.tag.name || ' '
+    if (t.tag.icon) {
+      photo_name = t.tag.icon + photo_name
+    }
+    photos.push({
+      name: photo_name,
+      src: photo,
+      rating: t.rating,
+    })
+    if (photos.length >= max_photos) break
+  }
+  if (photos.length <= max_photos) {
+    const restPhotos = restaurant.photos() ?? []
+    for (const photo of restPhotos) {
+      photos.push({ name: ' ', src: photo })
+      if (photos.length >= max_photos) break
+    }
+  }
+  return photos
 }
