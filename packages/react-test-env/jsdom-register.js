fdd9b98b994dbf7d21de055efbe31d3d34c65d0d
natew <natewienert@gmail.com>
2020-05-30 22:36:14 -0700

    [graph] fix broken test logic

---
+++ packages/react-test-env/jsdom-register.js
@@ -0,0 +1,18 @@
+const { JSDOM } = require('jsdom')
+
+const url = 'http://d1sh_hasura_live.com:19006/'
+
+// fake a browser!
+const jsdom = new JSDOM(``, {
+  pretendToBeVisual: true,
+  url: url,
+  referrer: url,
+  contentType: 'text/html',
+})
+
+global['window'] = jsdom.window
+Object.keys(jsdom.window).forEach((key) => {
+  if (typeof global[key] === 'undefined') {
+    global[key] = jsdom.window[key]
+  }
+})
