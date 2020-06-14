4c5e3b254295ea4c361c5823958c93561b3e4715
natew <natewienert@gmail.com>
2020-06-14 02:48:35 -0700

    [web] squircle, fixup naming

--- apps/web/shared/views/home/DishView.tsx
+++ apps/web/shared/views/home/DishView.tsx
@@ -15,6 +15,7 @@ import { NavigableTag } from '../../state/Tag'
 import { LinkButton } from '../ui/LinkButton'
 import { LinkButtonProps } from '../ui/LinkProps'
 import { DishRatingView } from './DishRatingView'
+import { Squircle } from './Squircle'
 
 export const DishView = memo(
   ({
@@ -29,7 +30,6 @@ export const DishView = memo(
     size?: number
     restaurantSlug?: string
   } & StackProps) => {
-    const borderRadius = 40
     const [isHovered, setIsHovered] = useState(false)
 
     const linkButtonProps: LinkButtonProps = {
@@ -79,80 +79,25 @@ export const DishView = memo(
             />
           )}
         </ZStack>
-        {/* frame (shadow) */}
-        <VStack
-          width={size}
-          height={size}
-          shadowColor="rgba(0,0,0,0.032)"
-          shadowRadius={20}
-          shadowOffset={{ width: 0, height: 20 }}
-          borderRadius={borderRadius}
+        <Squircle
+          size={size}
+          isHovered={isHovered}
+          {...(dish.isFallback && {
+            opacity: 0.8,
+          })}
         >
-          {/* frame (inner) */}
-          <VStack
-            className="ease-in-out-fast"
-            shadowColor="rgba(0,0,0,0.2)"
-            shadowRadius={4}
-            shadowOffset={{ width: 0, height: 4 }}
-            flex={1}
-            borderRadius={borderRadius}
-            overflow="hidden"
-            alignItems="center"
-            justifyContent="center"
-            {...(dish.isFallback && {
-              opacity: 0.8,
-            })}
-            pointerEvents="none"
-            {...(isHovered && {
-              borderColor: 'rgba(0,0,0,0.75)',
-              backgroundColor: '#fff',
-              shadowRadius: 13,
-              shadowColor: 'rgba(0,0,0,0.17)',
-              shadowOffset: { width: 0, height: 6 },
-              zIndex: 10000,
-            })}
-          >
-            {/* <ZStack position="absolute" zIndex={3} bottom={-10} right={-10}>
-              <Text fontSize={50} color="red">
-                ♥️
-              </Text>
-            </ZStack> */}
-            <ZStack
-              className={
-                // isHovered
-                // 'ease-in-out inner-glow'
-                'ease-in-out inner-shadow'
-              }
-              borderRadius={borderRadius}
-              overflow="hidden"
-              fullscreen
-              zIndex={2}
-            >
-              <LinearGradient
-                colors={[
-                  'rgba(255,255,255,0.45)',
-                  'rgba(255,255,255,0)',
-                  'rgba(255,255,255,0)',
-                  'rgba(0,0,0,0)',
-                  'rgba(30,30,30,0.2)',
-                ]}
-                style={[StyleSheet.absoluteFill]}
-              />
-            </ZStack>
-            {!!dish.image && (
-              <Image
-                source={{ uri: dish.image }}
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-                resizeMode="cover"
-              />
-            )}
-            {!dish.image && <Text fontSize={80}>🥗</Text>}
-          </VStack>
-        </VStack>
-
+          {!!dish.image && (
+            <Image
+              source={{ uri: dish.image }}
+              style={{
+                width: '100%',
+                height: '100%',
+              }}
+              resizeMode="cover"
+            />
+          )}
+          {!dish.image && <Text fontSize={80}>🥗</Text>}
+        </Squircle>
         <HStack
           position="absolute"
           bottom={-15}
