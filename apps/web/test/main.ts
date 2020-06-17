d0ffb2134df3a326b610fccafa675f6f7e614a8a
natew <natewienert@gmail.com>
2020-06-17 16:03:12 -0700

    [web] log more information on failure

--- apps/web/test/main.ts
+++ apps/web/test/main.ts
@@ -5,7 +5,20 @@ fixture('Home page').page('http://localhost:19006/')
 test('Basic rendering check', async (t) => {
   const divs = Selector('div')
   const numDivs = await divs.count
-  await t.expect(numDivs).gte(50)
+  try {
+    await t.expect(numDivs).gte(50)
+  } catch (err) {
+    console.log('error', err.message)
+    const body = Selector('body')
+    console.log(
+      'failure notes:',
+      await divs.innerText,
+      await body.childNodeCount,
+      await body.innerText
+    )
+    const out = await fetch('http://localhost:19006/').then((res) => res.text())
+    console.log('failure out', out)
+  }
 })
 
 // test('Renders home top dishes', async (t) => {
