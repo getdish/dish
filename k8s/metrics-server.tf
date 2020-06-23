0406a735133032eefe0f96e4faabe24afe8666c6
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-06-23 16:26:37 +0200

    [k8s] Metrics Server
    Gives us better insight into the CPU and RAM usage of our pods

---
+++ k8s/metrics-server.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "metrics-server" {
+  name = "metrics-server"
+  namespace = "metrics-server"
+  chart = "stable/metrics-server"
+  version = "2.11.1"
+
+  values = [
+    file("yaml/metrics-server.yaml")
+  ]
+}
