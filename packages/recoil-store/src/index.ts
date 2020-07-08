64ef3771622e23542816f676d32bbe73fec33d22
natew <natewienert@gmail.com>
2020-07-08 00:57:55 -0700

    [typescript] 4

--- packages/recoil-store/src/index.ts
+++ packages/recoil-store/src/index.ts
@@ -179,6 +179,7 @@ function getStoreDescriptors(storeInstance: any) {
     ...protoDescriptors,
     ...instanceDescriptors,
   }
+  // @ts-ignore
   delete descriptors.constructor
   return descriptors
 }
