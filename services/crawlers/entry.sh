25282d8da2eeac0ddd7f2fc0214fec9f09d337e2
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-07-09 16:33:32 +0300

    [crawlers] Better cron loggging

--- services/crawlers/entry.sh
+++ services/crawlers/entry.sh
@@ -2,5 +2,11 @@
 
 printenv | grep -v "no_proxy" >> /etc/environment
 
+# Rsyslog is needed for cron to log to
+service rsyslog start
+
+# List all current cron jobs
+crontab -l
+
 echo "Starting crawler scheduler..."
-/usr/sbin/cron -f -L 8
+/usr/sbin/cron -f -L 15
