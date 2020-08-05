892453454fa6ce99e31ebaabdd58a720a871666e
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-08-05 12:28:03 +0300

    [timescale] Conditionally use SSL connection

--- services/timescaledb/migrate.sh
+++ services/timescaledb/migrate.sh
@@ -2,8 +2,14 @@
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
+if [[ $DISH_ENV == 'production' ]]; then
+  USE_SSL=true
+else
+  USE_SSL=false
+fi
+
 pushd $PROJECT_ROOT/services/timescaledb
-USE_SSL=true $PROJECT_ROOT/node_modules/.bin/postgrator \
+$PROJECT_ROOT/node_modules/.bin/postgrator \
   --host ${PG_HOST:-localhost} \
   --port ${PG_PORT:-5433} \
   --database ${PG_DATABASE:-scrape_data} \
