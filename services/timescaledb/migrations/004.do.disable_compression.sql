7a9c696565230b6eae5fc430bd8a7c6bc7e3cafe
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-08-27 17:35:00 +0300

    [timescale] Tweak compression disabling migration
    So it works on local dev too

--- services/timescaledb/migrations/004.do.disable_compression.sql
+++ services/timescaledb/migrations/004.do.disable_compression.sql
@@ -4,9 +4,11 @@ SELECT decompress_chunk(i.chunk_name) FROM (
   SELECT chunk_name
   FROM timescaledb_information.compressed_chunk_stats
   WHERE compression_status = 'Compressed'
-) i
+) i;
 
-ALTER TABLE scrape SET (timescaledb.compress=false);
+UPDATE _timescaledb_catalog.hypertable
+  SET compressed = false
+  WHERE table_name = 'scrape';
 
 END;
 
