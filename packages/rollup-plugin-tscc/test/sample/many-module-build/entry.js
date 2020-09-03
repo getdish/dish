410144fff5eafee40b8d98c9b66a47c90053c392
natew <natewienert@gmail.com>
2020-09-03 00:53:01 -0700

    :fire: remove old experiments

--- packages/rollup-plugin-tscc/test/sample/many-module-build/entry.js
+++
@@ -1,9 +0,0 @@
-import {swap} from './common';
-
-const a = [1, 2];
-const b = swap(a);
-
-console.log(b);
-
-export { b };
-
--- packages/tscc/src/graph/ISourceNode.ts
+++
@@ -1,6 +0,0 @@
-export interface ISourceNode {
-  readonly fileName: string
-  readonly provides: ReadonlyArray<string>
-  readonly required: ReadonlyArray<string>
-  readonly forwardDeclared: ReadonlyArray<string>
-}
