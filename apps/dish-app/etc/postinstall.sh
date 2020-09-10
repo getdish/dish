1baca980c077789ada355a54b71b6f675832f06f
natew <natewienert@gmail.com>
2020-09-10 12:06:47 -0700

    [web] upgrade to newest react

---
+++ apps/dish-app/etc/postinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")/.."
+
+rm -r ../../node_modules/react-native || true
+rm -r ../../node_modules/react || true
+rm -r ../../node_modules/react-dom || true
+ln -s $(realpath ./node_modules/react-native) ../../node_modules
+ln -s $(realpath ./node_modules/react) ../../node_modules
+ln -s $(realpath ./node_modules/react-dom) ../../node_modules
