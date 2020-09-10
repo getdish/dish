42107e9de3aaf05e68bef70382de4033b88c008b
natew <natewienert@gmail.com>
2020-09-10 00:49:16 -0700

    [web] fixes for running rn more smoothly

--- apps/dish-app/App.tsx
+++ apps/dish-app/App.tsx
@@ -1,6 +1,7 @@
 import { AbsoluteVStack, VStack } from '@dish/ui'
+import { SplashScreen } from 'expo'
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Constants } from 'react-native-unimodules'
 
@@ -9,15 +10,19 @@ import { MapNative } from './shared/views/MapNative'
 console.log(Constants.systemFonts)
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hideAsync()
+  }, [])
+
   return (
     <View style={styles.container}>
       <MapNative />
 
-      <AbsoluteVStack>
-        {/* <DrawerNative> */}
+      {/* <AbsoluteVStack>
+        <DrawerNative>
         <VStack width={100} height={100} />
-        {/* </DrawerNative> */}
-      </AbsoluteVStack>
+        </DrawerNative>
+      </AbsoluteVStack> */}
       <StatusBar style="auto" />
     </View>
   )
