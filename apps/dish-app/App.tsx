af106b99cca3553e59d17f56f00d910fc2337ec8
natew <natewienert@gmail.com>
2020-09-09 18:13:52 -0700

    [web] add mapbox ios

--- apps/dish-app/App.tsx
+++ apps/dish-app/App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Constants } from 'react-native-unimodules'
+
+import { MapNative } from './shared/views/MapNative'
+
+console.log(Constants.systemFonts)
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <MapNative />
       <StatusBar style="auto" />
     </View>
   )
