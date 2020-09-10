9d2cd9aed70bcdcebe971e6e6329c756a8c67979
natew <natewienert@gmail.com>
2020-09-09 20:02:37 -0700

    [web] start more zoomed in

--- apps/dish-app/shared/state/getDefaultLocation.ts
+++ apps/dish-app/shared/state/getDefaultLocation.ts
@@ -1,20 +1,25 @@
 import { getLocalJSON, setLocalJSON } from './getLocalJSON'
 import { HomeStateItemLocation } from './HomeStateItemLocation'
 
+const initSpan = {
+  lng: 0.15 / 2,
+  lat: 0.15,
+}
+
 export function getDefaultLocation(): HomeStateItemLocation {
   const location = getLocalJSON('DEFAULT_LOCATION', {
     center: {
       lng: -122.421351,
       lat: 37.759251,
     },
-    span: { lng: 0.2 / 2, lat: 0.2 },
+    span: initSpan,
   })
   return {
     ...location,
     span: {
       // minimum span for initial location
-      lng: Math.max(0.2, location.span.lng),
-      lat: Math.max(0.2, location.span.lat),
+      lng: Math.max(initSpan.lng, location.span.lng),
+      lat: Math.max(initSpan.lat, location.span.lat),
     },
   }
 }
