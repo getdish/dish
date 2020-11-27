eedb850692711135660256543cfde9f5f12ca987
natew <natewienert@gmail.com>
2020-11-27 14:55:09 -0700

    [app] fix a few dish select issues

--- dish-app/shared/helpers/selectDishViewSimple.ts
+++ dish-app/shared/helpers/selectDishViewSimple.ts
@@ -1,13 +1,25 @@
 import { RestaurantTag, Tag } from '@dish/graph'
 
-export const selectRishDishViewSimple = (tag: RestaurantTag) => {
+export type DishTagItemSimple = {
+  id: string
+  name: string
+  icon?: string
+  score?: number
+  image: string
+  slug: string
+  isFallback?: boolean
+}
+
+export const selectRishDishViewSimple = (
+  tag: RestaurantTag
+): DishTagItemSimple => {
   const tagImage = tag.photos()?.[0]
   const tagFallbackImage = tagImage ? null : tag.tag?.default_images()?.[0]
   return {
-    id: tag.tag.id,
-    name: tag.tag.name ?? '',
-    icon: tag.tag.icon ?? '',
-    slug: tag.tag.slug ?? '',
+    id: tag.tag?.id,
+    name: tag.tag?.name ?? '',
+    icon: tag.tag?.icon ?? '',
+    slug: tag.tag?.slug ?? '',
     score: tag.score ?? 0,
     image: tagImage ?? tagFallbackImage,
     isFallback: !tagImage,
