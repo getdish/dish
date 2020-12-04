8a72a140770a2334c3c71c7732b49a06dd2edd2f
natew <natewienert@gmail.com>
2020-12-03 21:01:34 -0800

    [app] use eval source map

--- dish-app/webpack.config.js
+++ dish-app/webpack.config.js
@@ -63,7 +63,10 @@ module.exports = function getWebpackConfig(
         : [],
       stats: 'normal',
       // @ts-ignore
-      devtool: env.mode === 'production' ? 'source-map' : 'cheap-source-map',
+      devtool:
+        env.mode === 'production'
+          ? 'source-map'
+          : 'eval-cheap-module-source-map',
       //'eval-cheap-module-source-map' is fast but debugging original source becomes hard
       entry: {
         main: process.env.LEGACY ? ['babel-polyfill', appEntry] : appEntry,
