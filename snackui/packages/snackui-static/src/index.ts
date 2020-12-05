a3d7264aeaa6a43300c9ab3c9249589c08af057d
natew <natewienert@gmail.com>
2020-12-04 20:45:40 -0800

    [snackui] (wip) babel => stylesheet extraction

--- snackui/packages/snackui-static/src/index.ts
+++ snackui/packages/snackui-static/src/index.ts
@@ -2,3 +2,5 @@ process.env.SNACKUI_COMPILE_PROCESS = '1'
 
 export { default as GlossWebpackLoader } from './loader'
 export * from './getStylesAtomic'
+export { createExtractor } from './ast/createExtractor'
+export { literalToAst } from './ast/literalToAst'
