460b3523841bf2227f3b269365c7d0d99994f0df
natew <natewienert@gmail.com>
2020-12-06 02:02:14 -0800

    [scripts] postinstall faster

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -4,7 +4,7 @@ set -e
 PROJECT_ROOT="$(dirname "$0")/.."
 
 pushd $PROJECT_ROOT/dish-app
-yarn patch-package
+yarn patch-package &
 popd
 pushd $PROJECT_ROOT/node_modules
 rm -r react-native || true
@@ -12,12 +12,15 @@ rm -r react || true
 rm -r react-dom || true
 rm -r react-native-svg || true
 rm -r react-native-web || true
+rm -r snackui/node_modules/@o || true
+rm -r snackui/node_modules/@dish || true
 ln -s ../dish-app/node_modules/react-native .
 ln -s ../dish-app/node_modules/react .
 ln -s ../dish-app/node_modules/react-dom .
 ln -s ../dish-app/node_modules/react-native-svg .
 ln -s ../dish-app/node_modules/react-native-web .
-yarn build:refs
-yarn expo:check-deps
+yarn build:refs &
+yarn expo:check-deps &
+wait
 popd
 
