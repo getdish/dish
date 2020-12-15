50a3e55e7ac0311106659437db42e18c246b43e1
PabloSzx <pablosaez1995@gmail.com>
2020-12-15 02:47:41 -0300

    update gqless cli

--- packages/graph/generate.ts
+++ packages/graph/generate.ts
@@ -9,36 +9,7 @@ const run = async () => {
     destination: 'src/graphql/new-generated/index.ts',
     overwrite: true,
     headers: Auth.getHeaders(),
-    generateOptions: {
-      preImport: `
-      import { Auth } from '../../Auth'
-      import { getGraphEndpoint } from '../../helpers/getGraphEndpoint'
-      `,
-      queryFetcher: `
-      const queryFetcher: QueryFetcher = async function (query, variables) {
-        const response = await fetch(getGraphEndpoint(), {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            ...Auth.getHeaders(),
-          },
-          body: JSON.stringify({
-            query,
-            variables,
-          }),
-          mode: 'cors',
-        })
-      
-        if (!response.ok) {
-          throw new Error(\`Network error, received status code \${response.status}\`)
-        }
-      
-        const json = await response.json()
-      
-        return json
-      }
-      `,
-    },
+    generateOptions: {},
   })
 }
 
