1001f82f0b452996301c82eee171ee7618bb1373
natew <natewienert@gmail.com>
2021-01-16 23:55:00 -0700

    [snackui-static] fix bugs with memfs

--- packages/server/src/lib/getWebpackConfigBuilder.tsx
+++ packages/server/src/lib/getWebpackConfigBuilder.tsx
@@ -3,7 +3,12 @@ import { join } from 'path'
 import { CreateWebpackConfig } from '../types'
 
 export function getWebpackConfigBuilder({ rootDir }: { rootDir: string }) {
-  return require(join(rootDir, 'webpack.config.js')) as (
-    config: CreateWebpackConfig
-  ) => any
+  try {
+    return require(join(rootDir, 'webpack.config.js')) as (
+      config: CreateWebpackConfig
+    ) => any
+  } catch (err) {
+    console.error('No webpack.config.js')
+    process.exit(1)
+  }
 }
