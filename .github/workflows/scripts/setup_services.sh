9b7b3577760e549651d02f7443bf5bf2e6bda990
natew <natewienert@gmail.com>
2021-01-19 14:32:29 -0700

    fix builds

--- .github/workflows/scripts/setup_services.sh
+++ .github/workflows/scripts/setup_services.sh
@@ -4,6 +4,7 @@ set -x
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 export DISH_BASE_VERSION=:${branch//\//-}
+export DISH_IMAGE_TAG=latest
 
 is_hasura_up() {
   [ $(curl -L $HASURA_ENDPOINT/healthz -o /dev/null -w '%{http_code}\n' -s) == "200" ]
@@ -24,15 +25,11 @@ mkdir -p $HOME/.dish/postgres/data
 docker-compose up -d postgres
 sleep 10 # Postgres needs 2 starts to get everything set up
 docker-compose down
-docker-compose up -d hasura postgres timescaledb
+./dishctl.sh docker_compose_up_for_tests -d
 
 ./dishctl.sh db_migrate_local init
 docker run --net host $DISH_REGISTRY/base \
   bash -c 'cd services/timescaledb && DISH_ENV=not-production ./migrate.sh'
-# JWT server won't start until migrations have been applied
-docker-compose down
-
-./dishctl.sh docker_compose_up_for_tests -d
 
 if ! timeout --preserve-status 20 bash -c wait_until_hasura_ready; then
   echo "Timed out waiting for Hasura container to start"
