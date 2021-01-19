b8b97ef8f4be8234f3eb678e8eab0bf8fef9ffbb
natew <natewienert@gmail.com>
2021-01-18 19:39:58 -0700

    [app] attempt fix node_env on docker build

--- .github/workflows/scripts/setup_deps.sh
+++ .github/workflows/scripts/setup_deps.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
-set -x
 
 yarn global add hasura-cli@v1.2.2
-yarn global add https://github.com/getdish/postgrator-cli
+yarn global add @dish/postgrator-cli
 
 ./.github/workflows/scripts/decrypt_creds.sh
 ./.github/workflows/scripts/setup_admin.sh
