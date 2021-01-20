70ad87240be421ba6251db5e40eb52af2fdd0729
natew <natewienert@gmail.com>
2021-01-19 20:47:58 -0700

    [app] remove polyfill no longer needed

--- dish-app/src/web/polyfill.legacy.js
+++
@@ -1,5 +0,0 @@
-window['requestIdleCallback'] = window['requestIdleCallback'] || setTimeout
-window['ResizeObserver'] = require('resize-observer-polyfill').default
-// @ts-ignore
-require('array-flat-polyfill')
-console.log('hi legacy browser')
