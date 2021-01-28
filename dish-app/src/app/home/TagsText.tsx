fa3f5e73b3eee544be2a3a8d4efebc2cb94c0fd9
natew <natewienert@gmail.com>
2021-01-28 13:06:27 -0700

    [app] home feed nicer cuisine

---
+++ dish-app/src/app/home/TagsText.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Text } from 'snackui'
+
+import { tagDisplayName } from '../../constants/tagMeta'
+
+export function TagsText({ tags, color }: { tags: any[]; color?: string }) {
+  return (
+    <>
+      {tags.map((tag, index) => (
+        <React.Fragment key={tag.name}>
+          {tag.icon ? (
+            <Text
+              marginRight={6}
+              fontSize={20}
+              lineHeight={20}
+              transform={[{ translateY: 0.5 }]}
+            >
+              {tag.icon.trim()}{' '}
+            </Text>
+          ) : (
+            ''
+          )}
+          <Text color={color} fontSize={16} fontWeight="500">
+            {tagDisplayName(tag)}
+          </Text>
+          {index < tags.length - 1 ? (
+            <Text
+              paddingHorizontal={8}
+              fontWeight="700"
+              fontSize={12}
+              opacity={0.23}
+              transform={[{ translateY: -3 }]}
+            >
+              +
+            </Text>
+          ) : null}
+        </React.Fragment>
+      ))}
+    </>
+  )
+}
