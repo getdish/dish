960a9fedae7167425e49625920c93fa59f5c1c3c
natew <natewienert@gmail.com>
2021-01-29 16:32:41 -0700

    [app] list colors

--- dish-app/src/app/home/list/listColors.tsx
+++ dish-app/src/app/home/list/listColors.tsx
@@ -9,3 +9,10 @@ export const listColors = [...allColors, ...allColorsPastel, ...allDarkColor]
 export function getListColor(index?: number | null) {
   return listColors[index ?? 0]
 }
+
+export function randomListColor() {
+  return Math.min(
+    Math.floor(Math.random() * listColors.length),
+    listColors.length - 1
+  )
+}
