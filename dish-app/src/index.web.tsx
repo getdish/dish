bcfe06ca8db3932135bac809e107f287c41a57a5
natew <natewienert@gmail.com>
2021-02-16 14:59:24 -0700

    [app] restore index

--- dish-app/src/index.web.tsx
+++ dish-app/src/index.web.tsx
@@ -1,57 +1,53 @@
-import * as S from 'react-native-svg'
+import './web/base.css'
 
-console.log('S', S)
+import { startLogging } from '@dish/graph'
+import { loadableReady } from '@loadable/component'
+import React from 'react'
+import { hydrate, render } from 'react-dom'
+import { AppRegistry } from 'react-native'
 
-// import './web/base.css'
+import { isSSR } from './constants/constants'
+import { Root } from './Root'
 
-// import { startLogging } from '@dish/graph'
-// import { loadableReady } from '@loadable/component'
-// import React from 'react'
-// import { hydrate, render } from 'react-dom'
-// import { AppRegistry } from 'react-native'
+if (process.env.NODE_ENV === 'development' && !window['STARTED']) {
+  startLogging()
+}
 
-// import { isSSR } from './constants/constants'
-// import { Root } from './Root'
+const IS_CONCURRENT = window.location.search.indexOf(`concurrent`) > -1
+const ROOT = document.getElementById('root')
 
-// if (process.env.NODE_ENV === 'development' && !window['STARTED']) {
-//   startLogging()
-// }
+// register root component
+AppRegistry.registerComponent('dish', () => Root)
 
-// const IS_CONCURRENT = window.location.search.indexOf(`concurrent`) > -1
-// const ROOT = document.getElementById('root')
+async function start() {
+  if (IS_CONCURRENT) {
+    console.warn('👟 Concurrent Mode Running')
+    // @ts-expect-error
+    React.unstable_createRoot(ROOT).render(<Root />)
+    return
+  }
 
-// // register root component
-// AppRegistry.registerComponent('dish', () => Root)
+  if (process.env.NODE_ENV === 'production') {
+    loadableReady(() => {
+      hydrate(<Root />, ROOT)
+    })
+  } else {
+    render(<Root />, ROOT)
+  }
+}
 
-// async function start() {
-//   if (IS_CONCURRENT) {
-//     console.warn('👟 Concurrent Mode Running')
-//     // @ts-expect-error
-//     React.unstable_createRoot(ROOT).render(<Root />)
-//     return
-//   }
+// SSR exports
+if (isSSR) {
+  exports.App = Root
+  exports.ReactDOMServer = require('react-dom/server')
+}
 
-//   if (process.env.NODE_ENV === 'production') {
-//     loadableReady(() => {
-//       hydrate(<Root />, ROOT)
-//     })
-//   } else {
-//     render(<Root />, ROOT)
-//   }
-// }
+if (process.env.NODE_ENV === 'development') {
+  // @ts-expect-error
+  module?.hot?.accept()
+}
 
-// // SSR exports
-// if (isSSR) {
-//   exports.App = Root
-//   exports.ReactDOMServer = require('react-dom/server')
-// }
-
-// if (process.env.NODE_ENV === 'development') {
-//   // @ts-expect-error
-//   module?.hot?.accept()
-// }
-
-// if (!window['STARTED'] && process.env.TARGET !== 'node') {
-//   start()
-//   window['STARTED'] = true
-// }
+if (!window['STARTED'] && process.env.TARGET !== 'node') {
+  start()
+  window['STARTED'] = true
+}
