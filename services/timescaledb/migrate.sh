f7a1e3fbbb85592f484071822515d8a3d30db30a
natew <natewienert@gmail.com>
2021-02-23 23:23:03 -0700

    fix migrate

--- services/timescaledb/migrate.sh
+++ services/timescaledb/migrate.sh
@@ -12,10 +12,5 @@ else
 fi
 
 pushd $path
-USE_SSL=$USE_SSL ./node_modules/.bin/postgrator \
-  --host ${PG_HOST:-localhost} \
-  --port ${PG_PORT:-5433} \
-  --database ${PG_DATABASE:-scrape_data} \
-  --username ${PG_USER:-postgres} \
-  --password ${PG_PASS:-postgres}
+USE_SSL=$USE_SSL ./services/timescaledb/bin/migrate
 popd
