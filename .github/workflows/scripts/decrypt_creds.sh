4241ff21484d1d74d1efeb8865f1dcfa65605fec
natew <natewienert@gmail.com>
2021-02-27 06:17:52 -0700

    try diff decrypt

--- .github/workflows/scripts/decrypt_creds.sh
+++ .github/workflows/scripts/decrypt_creds.sh
@@ -3,11 +3,13 @@ set -e
 set -x
 
 echo "Decrypting Dish secrets..."
+
 KEY_PATH=/tmp/gitcrypt-key
+
 apt-get install -y git-crypt
-echo $GITCRYPT_KEY
-echo $GITCRYPT_KEY > $KEY_PATH.base64
-cat $KEY_PATH.base64 | base64 -d > $KEY_PATH
+
+echo "$GPG_KEY" > key
+gpg --import key
+gpg --list-keys
 git reset --hard
-cat $KEY_PATH
-git-crypt unlock $KEY_PATH
+git-crypt unlock
