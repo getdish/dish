5e7b9c115bdbfe026c175f6bcf9e315363951c53
natew <natewienert@gmail.com>
2021-03-01 02:05:40 -0700

    fix test integration

--- dish-app/test/testcafe.sh
+++ dish-app/test/testcafe.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-testcafe_bin=../node_modules/.bin/testcafe
-
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 pushd $PROJECT_ROOT/dish-app
 
 export NODE_ENV=test
 
-$testcafe_bin \
-  chrome:headless \
+docker run -v $PROJECT_ROOT/dish-app/test:/tests -it testcafe/testcafe "chromium:headless" \
   --skip-js-errors \
   --screenshots $PROJECT_ROOT/dish-app/test/screenshots \
-  --screenshots-on-fails \
-  test
+  --screenshots-on-fails
+
 popd
