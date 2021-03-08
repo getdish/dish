3a7d6a1aea4863cabbf4678919530c9b5bf85353
natew <natewienert@gmail.com>
2021-03-08 14:22:43 -0700

    fix envs

--- dish-app/.ci/pre_deploy.sh
+++ dish-app/.ci/pre_deploy.sh
@@ -9,4 +9,5 @@ fi
 flyctl secrets set \
     TF_VAR_DO_SPACES_ID="$DO_SPACES_ID" \
     TF_VAR_DO_SPACES_SECRET="$DO_SPACES_SECRET" \
+    HASURA_ENDPOINT="https://hasura.dishapp.com" \
     || true
