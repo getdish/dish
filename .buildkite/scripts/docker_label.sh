2b1ddc1679efff22af47222d4da5b940944bc48b
natew <natewienert@gmail.com>
2021-03-09 03:19:04 -0700

    save

--- .buildkite/scripts/docker_label.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-docker tag \
-  "gcr.io/dish-258800/$BUILDKITE_STEP_KEY/$BUILDKITE_PIPELINE_NAME-$BUILDKITE_STEP_KEY-build-$BUILDKITE_BUILD_ID" \
-  "gcr.io/dish-258800/$BUILDKITE_STEP_KEY:latest"
