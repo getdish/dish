8b1c91efc7eb0425a830ea68888ba60795bf5053
natew <natewienert@gmail.com>
2021-04-09 16:26:45 -0700

    [site] home add 1 git-crypt collaborator [env] compat with source add dedicated ssh keys and git-crypt

--- .buildkite/scripts/deploy_all.sh
+++ .buildkite/scripts/deploy_all.sh
@@ -6,7 +6,38 @@ echo "Deploying apps..."
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 cd "$PROJECT_ROOT"
 
-# deploy
+# deploy dedicated
+function deploy_dedicated_server() {
+  PRIVATE_KEY="$PROJECT_ROOT/etc/keys/d1_reliablesite_dish"
+  DEDICATED_APPS="dish-app-web dish-hooks search tileserver worker"
+  DISH_IMAGE_TAG=":latest"
+  SERVER_HOST="104.243.45.240"
+  rsync \
+    -avP \
+    --filter=':- .gitignore' \
+    -e "ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY" \
+    . "root@$SERVER_HOST:/app"
+  ssh \
+    -i "$PRIVATE_KEY" \
+    -o StrictHostKeyChecking=no \
+    "root@$SERVER_HOST" "
+      set -e
+      docker system prune --force
+      cd /app
+      git checkout nate/dev
+      source .env
+      source .env.production
+      docker-compose stop $DEDICATED_APPS
+      docker-compose rm -f dish-app-web dish-hooks search worker || true
+      ./dishctl.sh docker_pull_images_that_compose_would_rather_build
+      DISH_IMAGE_TAG=$DISH_IMAGE_TAG docker-compose up -d $DEDICATED_APPS
+    "
+  echo "success"
+}
+
+deploy_dedicated_server
+
+# deploy fly
 ./dishctl.sh deploy_all
 
 # post to slack
