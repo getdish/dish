e948194f3a7f1c2611101c40fa6f8c22b8056262
natew <natewienert@gmail.com>
2021-04-27 17:10:25 -0700

    save

--- services/timescale/scripts/migrate.js
+++ services/timescale/scripts/migrate.js
@@ -1,16 +1,14 @@
-#!/usr/bin/env node
-
 const Postgrator = require('postgrator')
 const { join } = require('path')
 
 const config = {
   migrationDirectory: join(__dirname, '..', 'migrations'),
   driver: 'pg',
-  host: process.env.PG_HOST || 'localhost',
-  port: +(process.env.PG_PORT || '5433'),
-  database: process.env.PG_DATABASE || 'scrape_data',
-  username: process.env.PG_USER || 'postgres',
-  password: process.env.PG_PASS || 'postgres',
+  host: process.env.TIMESCALE_HOST || 'localhost',
+  port: +(process.env.TIMESCALE_PORT || '5433'),
+  database: process.env.TIMESCALE_DATABASE || 'scrape_data',
+  username: process.env.TIMESCALE_USER || 'postgres',
+  password: process.env.TIMESCALE_PASS || 'postgres',
   ssl: process.env.USE_SSL === 'true' ? true : false,
 }
 
@@ -18,7 +16,17 @@ const config = {
 
 const postgrator = new Postgrator(config)
 
-postgrator
-  .migrate()
-  .then((appliedMigrations) => console.log('up to date', appliedMigrations.length))
-  .catch((error) => console.error('Error migrating', error))
+console.log('migrating timescale...', process.env.NODE_ENV)
+
+async function main() {
+  try {
+    const applied = await postgrator.migrate()
+    console.log('up to date', applied.length)
+  } catch (err) {
+    console.error('Error migrating', err)
+    process.exit(1)
+  }
+  process.exit(0)
+}
+
+main()
