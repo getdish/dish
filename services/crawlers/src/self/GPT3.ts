ce8345605437467340780135d4ac85aa78bbe7f0
natew <natewienert@gmail.com>
2021-06-24 16:46:43 -0700

    use old gpt summary tempalte

--- services/crawlers/src/self/GPT3.ts
+++ services/crawlers/src/self/GPT3.ts
@@ -21,6 +21,21 @@ const presets = {
   },
 }
 
+export const pre_prompt = `Summarize nicely the following as though you were a smart, wry Pixar food critic summarizing your visit to a restaurant:
+---
+Solid Vietnamese restaurant with pricing almost on par with Orange County! The interior is very spacious. The menu is extensive, lots of protein combinations for the staple dishes. Lovely vermicelli (bún), pho, broken rice (com tam), and beef stew (bo kho). Seriously, is the SF version of Pho Lu (in Westminster, CA) or nah?! We shared the vermicelli with grilled chicken and shrimp ($10.75). The fish sauce was flavorful, albeit not spicy. The grilled chicken had a nice char to it, the noodles were still warm when we got home 30 minutes later. Overall, definitely would order again. Next time I'll skip the nearby boba places and get my boba here because their fruit shakes (including durian!) look delicious and c'mon, you know Viet places only use fresh, real fruit for them!
+"""
+Summary: Pricey but solid Vietnamese! Large menu, lots of protein combos, amazing bun, pho com tam, and bo kho. Of course fresh real fruit boba smoothies (including durian!). C'mon man! 
+---
+Hands down my favorite taco joint in San Francisco. I think this is one of the few, if not the only restaurant in SF that serves birria tacos -- they are delicious! The beef stew meat and broth on the side go so well together (remember to add the cheese!) The flavors are intense but not too overwhelming. Another plus: these are HUGE tacos; I can only eat 3 at a time. It's always packed. You order at the counter first then wait for a table to open up. You have to strategize and be quick but still be respectful of others - absolutely not a time to be shy. They throw in complimentary chips and salsa with your order that are honestly great. PS: They also have sangria to-go now. It was perfectly concocted!
+"""
+Summary: Top tier tacos. Big Birria tacos with amazing flavor (the cheese is a must!). Absolutel UNIT of a taco! Hard to get a table sometimes, order at the counter. Tips: Sangria is to-go now, and decent free chips and salsa.
+---
+`
+
+export const post_prompt = `"""
+Summary:`
+
 // in order of most powerful => least
 type OpenAIEngines = 'davinci' | 'curie' | 'babbage' | 'ada'
 
@@ -76,12 +91,7 @@ export class GPT3 {
 
   async complete(input: string, engine: OpenAIEngines = 'curie', preset = 'witty_guidebook') {
     let body = presets[preset]
-    body.prompt = `A smart summarizer AI reads in the following:
-${input}
-"""
-It writes a short description with highlights of this restaurant:
-"""
-`
+    body.prompt = `${pre_prompt}${input}${post_prompt}`
     const request: RequestInit = {
       method: 'POST',
       headers: {
