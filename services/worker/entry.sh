e4456dc298a3f4f0295cc6f1aa8918e0d43fe5c4
natew <natewienert@gmail.com>
2021-07-23 18:11:14 -0700

    worker killall invalid

--- services/worker/entry.sh
+++ services/worker/entry.sh
@@ -6,9 +6,9 @@ if [ "$DISH_ENV" = "development" ]; then
   export CLEAR_JOBS
   ../../dsh run 'node dist/index.js'
 else
-  while true; do
-    sleep 1800
-    killall chrome
-  done &
+  # while true; do
+  #   sleep 1800
+  #   killall chrome
+  # done &
   node --expose-gc ./dist/index.js
 fi
