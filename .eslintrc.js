61c0248d26e7e4b148d6b0781851545534f054bb
natew <natewienert@gmail.com>
2021-08-19 13:52:27 -0700

    [app] fix profiles

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     'import/named': 'off',
     'react/no-unescaped-entities': 'off',
     'react/prop-types': 'off',
