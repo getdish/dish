90f221e85a8698aa28f9cf5a8d3b70729d9d2366
natew <natewienert@gmail.com>
2021-08-29 15:57:39 -0700

    [native] remove hermes

--- packages/graph/src/startLogging.ts
+++ packages/graph/src/startLogging.ts
@@ -13,17 +13,21 @@ export function startLogging(verbose = false) {
   if (process.env.NODE_ENV !== 'production') {
     if (isLogging) return
     isLogging = true
-    if (isSafari) {
-      console.log('Disable gqty logging as it doesnt collapse')
-    } else {
-      import('@gqty/logger').then((gqtyLogger) => {
-        if (!gqtyLogger?.createLogger) {
-          console.log('no logger', gqtyLogger)
-          return
-        }
-        const Logger = gqtyLogger.createLogger(client, { stringifyJSON: false })
-        Logger.start()
-      })
+
+    // disbale react native prettier dep uses named capture groups which breaks hermes
+    if (process.env.TARGET === 'web') {
+      if (isSafari) {
+        console.log('Disable gqty logging as it doesnt collapse')
+      } else {
+        import('@gqty/logger').then((gqtyLogger) => {
+          if (!gqtyLogger?.createLogger) {
+            console.log('no logger', gqtyLogger)
+            return
+          }
+          const Logger = gqtyLogger.createLogger(client, { stringifyJSON: false })
+          Logger.start()
+        })
+      }
     }
   }
 }
