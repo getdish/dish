347d3959f85455a03ccfb8408af9cfdbe18b256c
Thomas Buckley-Houston <tom@tombh.co.uk>
2021-09-02 12:30:44 -0500

    [crawlers] Minor Tripadvisor tweaks

--- services/crawlers/src/tripadvisor/sandbox.ts
+++ services/crawlers/src/tripadvisor/sandbox.ts
@@ -2,11 +2,12 @@ import { Restaurant, findOne } from '@dish/graph'
 
 import { Tripadvisor } from './Tripadvisor'
 
-const FLOUR = '/Restaurant_Review-g60713-d1516973-Reviews-Flour_Water-San_Francisco_California.html'
+const FLOUR = 'Restaurant_Review-g60713-d1516973-Reviews-Flour_Water-San_Francisco_California.html'
+const RPATH = '/' + (process.env.RPATH || FLOUR)
 
 async function one() {
   const t = new Tripadvisor()
-  await t.runOnWorker('getRestaurant', [FLOUR])
+  await t.runOnWorker('getRestaurant', [RPATH])
   await findOne<Restaurant>('restaurant', {
     name: 'Flour + Water',
   })
