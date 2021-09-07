0ef557908661ff725ba18d0849d9589dba8984db
natew <natewienert@gmail.com>
2021-09-06 18:17:58 -0700

    typescript 4.4

--- app/src/helpers/isIOS.tsx
+++ app/src/helpers/isIOS.tsx
@@ -1,4 +1,5 @@
 export const isWebIOS =
   (/iPad|iPhone|iPod/.test(navigator.platform) ||
     (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
-  !window.MSStream
+  typeof window !== 'undefined' &&
+  !window['MSStream']
