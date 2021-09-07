cbc237ccc9088b44b94cf6fb7d8ab01b633e5f94
natew <natewienert@gmail.com>
2021-09-07 12:13:44 -0700

    [app] sentry move to new account, share config, use git commit sha

--- app/index.js
+++ app/index.js
@@ -9,8 +9,8 @@ import { registerRootComponent } from 'expo'
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
-    dsn: 'https://e87d54b2bd5c4bec9d82304e2d4b71d1@o600766.ingest.sentry.io/5744061',
-    release: process.env.REACT_APP_COMMIT_HASH || process.env.COMMIT_HASH || 'unreleased',
+    dsn: process.env.SENTRY_URL || console.error('no sentry url'),
+    release: process.env.GIT_COMMIT || 'unreleased',
     environment: process.env.NODE_ENV || 'development',
   })
 }
--- app/src/web/sentry.js
+++ app/src/web/sentry.js
@@ -1,15 +1,9 @@
 import * as Sentry from '@sentry/browser'
 import { Integrations } from '@sentry/tracing'
 
-const release =
-  process.env.REACT_APP_COMMIT_HASH ||
-  process.env.COMMIT_HASH ||
-  process.env.npm_package_version ||
-  'unreleased'
-
 Sentry.init({
-  dsn: 'https://e87d54b2bd5c4bec9d82304e2d4b71d1@o600766.ingest.sentry.io/5744061',
-  release,
+  dsn: process.env.SENTRY_URL || console.error('no sentry url'),
+  release: process.env.GIT_COMMIT || 'unreleased',
   integrations: [new Integrations.BrowserTracing()],
   environment: process.env.NODE_ENV || 'development',
 })
