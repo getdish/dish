b10c283189dcd507741a1e88dcd25fb68e68ae44
natew <natewienert@gmail.com>
2021-09-08 08:35:53 -0700

    [app] update react

--- app/src/app/views/SuspenseFallback.tsx
+++ app/src/app/views/SuspenseFallback.tsx
@@ -1,28 +1,13 @@
-import React, { Suspense } from 'react'
-import { LoadingItem } from 'snackui'
+// @ts-ignore
+import React, { Suspense, SuspenseProps } from 'react'
 
-import { useStateSynced } from '../hooks/useStateSynced'
-
-export type SuspenseFallbackProps = { children: any; fallback?: any }
-
-export const SuspenseFallback = (props: SuspenseFallbackProps) => {
-  // disbaled for now
-  return <Suspense fallback={null} {...props} />
+export const SuspenseFallback = (props: Partial<SuspenseProps>) => {
+  return (
+    <>
+      <Suspense fallback={null} {...props} />
+    </>
+  )
 }
-
-// disabled needs to happen in react otherwise we have to compare deep trees every render
-
-// const FallbackRender = (props: SuspenseFallbackProps) => {
-//   const last = useStateSynced(props.children)
-//   const children = last ?? props.fallback
-
-//   if (typeof children === 'function') {
-//     console.error('got children as function', props, last)
-//     return null
-//   }
-
-//   return <>{children}</>
-// }
 
 export function suspenseFallback<A extends React.FunctionComponent<any>>(Component: A): A {
   return ((props) => {
