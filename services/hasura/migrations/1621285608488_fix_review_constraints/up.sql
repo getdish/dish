eedb1362bd898d6f297070a109655bb222285739
natew <natewienert@gmail.com>
2021-09-14 08:33:12 -0700

    [app] fix drop mutation

--- services/hasura/migrations/1621285608488_fix_review_constraints/up.sql
+++ services/hasura/migrations/1621285608488_fix_review_constraints/up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE ONLY public.review
-    DROP CONSTRAINT review_username_restaurant_id_tag_id_authored_at_key;
+    DROP CONSTRAINT IF EXISTS review_username_restaurant_id_tag_id_authored_at_key;
 
 ALTER TABLE ONLY public.review
    ADD CONSTRAINT review_username_restauarant_id_tag_id_type_key UNIQUE (username, restaurant_id, tag_id, type);
