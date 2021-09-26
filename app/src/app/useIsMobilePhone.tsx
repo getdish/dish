dfd7554bbac00e0f51c3487aba9b04d69a41c252
natew <natewienert@gmail.com>
2021-09-26 14:11:43 -0700

    [app] dont always show mobile phone

--- app/src/app/useIsMobilePhone.tsx
+++ app/src/app/useIsMobilePhone.tsx
@@ -6,6 +6,5 @@ import { getWindowHeight, getWindowWidth } from '../helpers/getWindow'
 const narrowestWindowSide = Math.min(getWindowWidth(), getWindowHeight())
 
 export const useIsMobilePhone = () => {
-  return true
   return narrowestWindowSide < 480 && isWeb && isTouchDevice
 }
