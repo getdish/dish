a752bd24e6cac615c24f824e5cda5bde7ac797c9
natew <natewienert@gmail.com>
2021-09-27 17:37:08 -0700

    fix babel config for test

--- packages/babel-preset/babel.config.js
+++ packages/babel-preset/babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
   const isSSR = process.env.TARGET === 'ssr'
   // NOTE: we dont use this for NATIVE, we use metro-babel
   const isLegacy = process.env.TARGET === 'native' || process.env.LEGACY === '1'
+  const isTest = process.env.NODE_ENV === 'test'
   const isDev = api.env('development') || process.env.NODE_ENV === 'development'
   const isProd = !isDev
 
@@ -33,24 +34,52 @@ module.exports = function (api) {
             '@babel/plugin-transform-react-constant-elements',
           ]
         : []),
-      'babel-plugin-react-native-web',
+      ...(isTest
+        ? [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-optional-chaining',
+            ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+            'babel-plugin-react-native-web',
+            '@babel/plugin-syntax-typescript',
+            '@babel/plugin-proposal-nullish-coalescing-operator',
+          ]
+        : []),
       isProd && require.resolve('./babel.strip-invariant.plugin.js'),
     ]
       .filter(Boolean)
       .map(resolvePlugin),
     presets: [
-      // [
-      //   '@babel/preset-react',
-      //   {
-      //     // auto adds react import if necessaty
-      //     runtime: 'automatic',
-      //     useBuiltIns: true,
-      //     development: isDev,
-      //     ...(isDev && {
-      //       importSource: '@welldone-software/why-did-you-render',
-      //     }),
-      //   },
-      // ],
+      ...(isTest
+        ? [
+            [
+              '@babel/preset-typescript',
+              { onlyRemoveTypeImports: true, isTSX: true, allExtensions: true },
+            ],
+            [
+              '@babel/preset-react',
+              {
+                // auto adds react import if necessaty
+                runtime: 'automatic',
+                useBuiltIns: true,
+                development: isDev,
+                ...(isDev && {
+                  importSource: '@welldone-software/why-did-you-render',
+                }),
+              },
+            ],
+            isLegacy && [
+              '@babel/preset-env',
+              {
+                useBuiltIns: 'usage',
+                corejs: 3,
+                targets: {
+                  browsers: ['>3%'],
+                },
+                exclude: ['@babel/plugin-transform-regenerator'],
+              },
+            ],
+          ]
+        : []),
     ]
       .filter(Boolean)
       .map(resolvePlugin),
