cebbdd35f2e431ab9ae1f9e31962e5a818c5e9be
natew <natewienert@gmail.com>
2021-10-06 20:11:51 -0700

    [crawlers] re enable better script

--- services/crawlers/scripts/crawl_one.ts
+++ services/crawlers/scripts/crawl_one.ts
@@ -1,25 +1,87 @@
-#!/bin/node
-
 import '@dish/helpers/polyfill'
 
-import { restaurantFindOne } from '@dish/graph'
+import { restaurantFindOne, restaurantUpdate } from '@dish/graph'
+import { scrape_db } from '@dish/helpers-node'
 
-import { removeScrapeForRestaurant } from '../src/scrape-helpers'
-import { one } from '../src/yelp/one'
+import * as Google from '../src/google/one'
+import * as Self from '../src/self/one'
+import * as Tripadvisor from '../src/tripadvisor/one'
+import * as Yelp from '../src/yelp/one'
 
-async function main(slug: string) {
-  if (!slug) {
-    throw new Error(`No slug`)
+export async function main(slug: string) {
+  try {
+    const rest = await restaurantFindOne({
+      slug,
+    })
+
+    if (!rest) {
+      console.log('No restaurant')
+      return
+    }
+
+    console.log(
+      'crawling restaurant',
+      `${slug} ${rest.id} ${rest.name} ${rest.address} ${rest.telephone}`
+    )
+
+    // reset scrape_metadata so it redoes summary
+    console.log('reset restaurant scrape metadata')
+    rest.scrape_metadata = {
+      ...rest.scrape_metadata,
+      gpt_summary_updated_at: null,
+    }
+    await restaurantUpdate(rest)
+
+    const skips = (process.env.SKIP ?? '').split(',')
+    const shouldSkip = (str: string) => skips.includes(str)
+
+    // clear existing scrapes
+    if (rest.id && !shouldSkip('external') && !shouldSkip('scrapes')) {
+      if (process.env.CLEAR_EXISTING === '1') {
+        const res = await scrape_db.query(`DELETE FROM scrape WHERE restaurant_id = '${rest.id}';`)
+        console.log('Deleted', res.rows)
+      } else {
+        console.log('To clear existing scrapes for restaurant set CLEAR_EXISTING=1')
+      }
+    }
+
+    if (!shouldSkip('external')) {
+      if (!shouldSkip('tripadvisor')) {
+        try {
+          await Tripadvisor.one(slug)
+        } catch (err) {
+          console.error(err)
+        }
+      }
+      if (!shouldSkip('yelp')) {
+        await Yelp.one(slug)
+      }
+      if (!shouldSkip('google')) {
+        await Google.one(slug)
+      }
+    }
+
+    if (!shouldSkip('internal')) {
+      await Self.one(slug)
+    }
+
+    console.log('done!')
+    process.exit(0)
+  } catch (err) {
+    console.error('error', err)
   }
-  const rest = await restaurantFindOne({
-    slug,
-  })
-  if (!rest) {
-    console.warn('not found', slug)
-    return
-  }
-  await removeScrapeForRestaurant(rest, 'yelp')
-  await one(slug)
 }
 
-main([...process.argv].reverse()[0])
+const slug = process.env.SLUG ?? process.argv[process.argv.length - 1]
+
+if (slug) {
+  main(slug)
+}
+
+// for dev it should quit background jobs better
+const cleanExit = () => {
+  console.log('clean exit')
+  process.exit(0)
+}
+process.on('SIGINT', cleanExit) // catch ctrl-c
+process.on('SIGTERM', cleanExit) // catch kill
