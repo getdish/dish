4f365aa6dfe26dafbd7050b27c4733b2cd359d2d
natew <natewienert@gmail.com>
2021-10-10 00:19:46 -0700

    clear force all

--- app/src/api/clearCache.ts
+++ app/src/api/clearCache.ts
@@ -2,12 +2,12 @@ import { CACHE_KEY_PREFIX, route } from '@dish/api'
 
 import { redisClient, redisDeletePattern } from './_redis'
 
-export default route((req, res) => {
+export default route(async (req, res) => {
   console.warn('Clearing REDIS cache')
   if (req.params.force) {
-    redisDeletePattern(``)
+    await redisClient.flushall()
   } else {
-    redisDeletePattern(`${CACHE_KEY_PREFIX}*`)
+    await redisDeletePattern(`${CACHE_KEY_PREFIX}*`)
   }
   res.send('ok')
 })
