bcd9bdfd9cc8c9b69595d6d6ca40b9e4d42894ee
natew <natewienert@gmail.com>
2022-05-08 00:05:10 -1000

    save

--- app/ios/dish/AppDelegate.mm
+++ app/ios/dish/AppDelegate.mm
@@ -42,7 +42,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"RnDiffApp", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"main", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
