7348b725cb131f18e37ac0a7f873441d6c8aaa53
natew <natewienert@gmail.com>
2022-07-16 14:51:49 -1000

    save

--- app/src/app/hooks/useLastValueWhen.tsx
+++ app/src/app/hooks/useLastValueWhen.tsx
@@ -2,19 +2,8 @@ import { useMemo, useRef } from 'react'
 
 // not concurrent safe
 
-export const useLastValueWhen = <A extends any = any>(
-  memoFn: () => A,
-  conditional: boolean,
-  debug?: string
-): A => {
+export function useLastValueWhen<A>(memoFn: () => A, conditional: boolean): A {
   const key = useRef(0)
-  if (!conditional) {
-    if (debug) {
-      console.log('useLastValueWhen: updating value', debug)
-    }
-    key.current = Math.random()
-  }
-  return useMemo(() => {
-    return memoFn()
-  }, [key.current])
+  if (!conditional) key.current = Math.random()
+  return useMemo(() => memoFn(), [key.current])
 }
